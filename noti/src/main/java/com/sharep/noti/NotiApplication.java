ea3795fe50c317a8e91f4aa1e44581430fd5a9c7
이승민 <mehot98@gmail.com>
2024-04-23 16:29:25 +0900

    [Add] 알림 서버 스켈레톤 프로젝트 생성

---
+++ noti/src/main/java/com/sharep/noti/NotiApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:30b82c635ed94b801379c293d810e37175bcd154ddfe084abd731ac515debfe1
+size 316
---
+++ noti/src/test/java/com/sharep/noti/NotiApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8a33be06f49864753fc1b944d5ccb5728fce7c205cd587b922dc1f2af73eab2
+size 214
