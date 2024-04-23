b28072e31bf1911509ac3383eca6dad9c9343936
오상훈 <ph38230119@gmail.com>
2024-04-23 15:41:11 +0900

    [Add] 스켈레톤 프로젝트 생성
    Former-commit-id: e5f989e038ca2973ff8c9f500f2633a815c8f653

---
+++ be/src/test/java/com/sharep/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharep.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
