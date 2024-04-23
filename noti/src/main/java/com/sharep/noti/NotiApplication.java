c8d9662d5e5b8a41baabcc9aa270b458fb3101ac
이승민 <mehot98@gmail.com>
2024-04-23 16:29:25 +0900

    [Add] 알림 서버 스켈레톤 프로젝트 생성
    Former-commit-id: 623b828727a234237c1dc7fabb6cb7279daf0418

---
+++ noti/src/main/java/com/sharep/noti/NotiApplication.java
@@ -0,0 +1,13 @@
+package com.sharep.noti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotiApplication.class, args);
+    }
+
+}
---
+++ noti/src/test/java/com/sharep/noti/NotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharep.noti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
