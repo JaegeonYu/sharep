210371904ba20daf7cf64dc231e2d42d097ec7cb
오상훈 <ph38230119@gmail.com>
2024-04-23 15:41:11 +0900

    [Add] 스켈레톤 프로젝트 생성

---
+++ be/src/main/java/com/sharep/be/BeApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f93c31e7666ca98611eca17c46c8503f193676ad68e5cedcd7e5e80354ca746b
+size 310
---
+++ be/src/test/java/com/sharep/be/BeApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2514040ad3b37e042323321c20eb14a4f447c767e73317402f84ca54141bd00
+size 210
