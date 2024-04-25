2cb8ba829c3f7836dd5339f0154de9651d3e221f
김성제 <rlatjdwp0211@naver.com>
2024-04-25 16:27:31 +0900

    [Add] Front 프로젝트 기초 생성
    - pakage.json
    - .prettierrc.json
    - dir 구조 잡기
    
    
    Former-commit-id: 7d8212b872ac1428a6a86c0d495dff1496b8e7ea

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:58a12e41994ea8bc9d40e1ef0afa0523e214992e4c2ad7ba363b7daf1bd152f3
-size 905
+import React from "react";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ fe/src/components/SideBar/style.ts
@@ -0,0 +1,1 @@
+export const SideBarWrapper = 1;
