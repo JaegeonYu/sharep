36f35e5c2b3a9b750a8107fd4b7cb6674b21a3b2
김성제 <rlatjdwp0211@naver.com>
2024-04-25 17:54:45 +0900

    [Style] 전역 스타일 설정
    - styles/index.ts 에 전역 스타일 설정
    - @type/styled-component pakage 추가
    
    
    Former-commit-id: abcf5a2ffb91bb2533251982e51080ed109df3fa

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eb566c2b337f8bb05287abc5dd494b89f37223b32f0480bac171dd02a55b557
-size 84
+oid sha256:5b6850d6e824dbabe17e657dafff6317974eac424a2a109e8dd18307b202d74f
+size 161
--- fe/src/vite-env.d.ts
+++ fe/src/vite-env.d.ts
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:65996936fbb042915f7b74a200fcdde7e410f32a669b1ab9597cfaa4b0faddb5
-size 38
+/// <reference types="vite/client" />
