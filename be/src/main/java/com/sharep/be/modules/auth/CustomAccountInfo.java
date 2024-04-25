8fe58a972f93152a603dc09fbfcbecc6b5f9fa17
jack <yjk9805@naver.com>
2024-04-25 09:12:28 +0900

    [Feat] 로그인 기능 구현
    Former-commit-id: d3e2c7fd18703f50f7a766b7a30a0d29abb760e1

--- be/src/main/java/com/sharep/be/modules/auth/CustomAccountInfo.java
+++ be/src/main/java/com/sharep/be/modules/auth/CustomAccountInfo.java
@@ -1,8 +1,12 @@
 package com.sharep.be.modules.auth;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
+@ToString
+@Setter
 public class CustomAccountInfo {
     private Long id;
     private String email;
