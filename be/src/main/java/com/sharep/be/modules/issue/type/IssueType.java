2f9b3f68aa9481dd742039cc2671afd79078b2f6
오상훈 <ph38230119@gmail.com>
2024-04-26 15:12:39 +0900

    [Feat] issue 엔터티 생성
    Former-commit-id: 33868b0938b9fc40c9647414ed0636fe94015c85

---
+++ be/src/main/java/com/sharep/be/modules/issue/type/IssueType.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8610da245f8db472510c68a0f26c3b5b8afd2cdbc6ac7682dcee04834ece487c
+size 100
---
+++ be/src/main/java/com/sharep/be/modules/issue/type/PriorityType.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1adebd162eb963048a2558ce6f5441ab8e8b7558ab71b310ec238f682aaa310a
+size 95
