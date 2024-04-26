9de2661a15d7a14788ac2e8465364502cc33b71f
김성제 <rlatjdwp0211@naver.com>
2024-04-26 14:35:25 +0900

    [Chore] StatusBadge 수정
    - BE 응답이 변경됨에 따라 props type과 상수 수정
    
    
    Former-commit-id: 004540d72edd4fe36c534efa2144acc10889beaa

--- fe/src/components/StatusBadge/StatusBadge.tsx
+++ fe/src/components/StatusBadge/StatusBadge.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0fdad5e1d87b16b934ebc2c3034076a79c648ea468df66bc1d637be73861cc3
-size 883
+oid sha256:f643a97117f47c34c5a76894bd31650f0f4a7304013aa83060e56c4f074fef7b
+size 769
--- fe/src/types/components/StatusBadge.ts
+++ fe/src/types/components/StatusBadge.ts
@@ -1,3 +1,3 @@
 export interface StatusBadgeProps {
-  status: '1' | '2' | '3';
+  status: '시작 전' | '진행 중' | '완료';
 }
