439b2c5e487310889edb537f80a3196904f671c7
김성제 <rlatjdwp0211@naver.com>
2024-04-26 10:34:24 +0900

    [Feat] 글로벌 세팅
    - ReactQueryProvider 추가
    - BrowserRouter 적용 후 Routes 세팅
    - 팀원들 UI 테스트 페이지 추가
    - RecoilRoot 추가
    
    
    Former-commit-id: c49c0426cb772ae842a87771f0741f4849dd6c22

---
+++ fe/src/pages/SungJe/SungJe.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SungJe() {
+  return <div>sung-je</div>;
+}
---
+++ fe/src/pages/index.ts
@@ -0,0 +1,5 @@
+import SeoJeong from './SeoJeong/SeoJeong';
+import SungGu from './SungGu/SungGu';
+import SungJe from './SungJe/SungJe';
+
+export { SeoJeong, SungGu, SungJe };
