6b699cf04aa5dc52b795c622c07688448986a63c
김성제 <rlatjdwp0211@naver.com>
2024-04-26 14:03:19 +0900

    [Style] MethodBadge 생성
    - Type, Style 생성
    
    
    Former-commit-id: 233903b39589fd1351eb2d7b371b40dbad481841

--- fe/src/pages/SungJe/SungJe.tsx
+++ fe/src/pages/SungJe/SungJe.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e53fa51e1e7680211fde2bf9c3d022160e52cf693d7702c31ffc9b97c0da8d9
-size 725
+oid sha256:7b12cb9d26c796f3ef07b38b712dfbb41d75d55902bf778d65ce5810a2395591
+size 922
---
+++ fe/src/types/components/MethodBadge.ts
@@ -0,0 +1,3 @@
+export interface MethodBadgeProps {
+  name: 'GET' | 'PUT' | 'PATCH' | 'POST' | 'DELETE';
+}
