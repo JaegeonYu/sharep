41ca7af6d0c57e4b47f5a399592c1a46bc3d7a13
김성제 <rlatjdwp0211@naver.com>
2024-04-26 13:30:24 +0900

    [Rename] InputWithLabel 타입 파일 이름 변경
    - input.ts -> InputWithLabel.ts
    
    
    Former-commit-id: 2cb87826c1e4f8add94ee32a695d1a552aa65b44

--- fe/src/types/index.ts
+++ fe/src/types/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8a8245500904ca930b38995740a7798608745588cba42dc16f76578aac2cee3
-size 168
+oid sha256:9ff9308a4521517ad82d819aca752ce18d765ff603f5f6dff73cd584f2b217e8
+size 256
