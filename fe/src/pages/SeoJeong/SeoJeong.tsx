90f56a524043552b77279345d966dabdf3acc817
임서정 <sjytis14@naver.com>
2024-04-26 13:23:29 +0900

    [Feat] Commit 컴포넌트 구현 완료
    Former-commit-id: d6ed0f141f39de43a2a4ace2cebd7399d14a158e

--- fe/src/pages/SeoJeong/SeoJeong.tsx
+++ fe/src/pages/SeoJeong/SeoJeong.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
-export default function SeoJeong() {
-  return <div>seo-jeong</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7bb705e657c100a368378bcbd0e526e0452d3cf94e2e908393873eeecf3ad12
+size 155
