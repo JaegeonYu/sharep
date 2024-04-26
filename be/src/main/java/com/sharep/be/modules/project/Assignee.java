4fe12ae2f359b94b992b5f05076db10dc720ac1f
jack <yjk9805@naver.com>
2024-04-26 15:07:23 +0900

    [Fix] Assignee State Enum으로 변경
    Former-commit-id: 75c84e7fb93368fc045be0f6ed2d7af47348096d

--- be/src/main/java/com/sharep/be/modules/project/Assignee.java
+++ be/src/main/java/com/sharep/be/modules/project/Assignee.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e16c532a965e17092df128be6e9d45214eb2cdef08977cdb160924b9126cebc
-size 821
+oid sha256:c89328f4adc9db87535fe3e1b57f2021bf1947fdc06b3709f541d49b051cbc32
+size 874
