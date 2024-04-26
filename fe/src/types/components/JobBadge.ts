413eb0075acd7b7231d48d9419f2492ec9826d1f
김성제 <rlatjdwp0211@naver.com>
2024-04-26 15:24:33 +0900

    [Feat] JobBadge 수정
    - BE 응답값 변경으로 인한 수정
    
    
    Former-commit-id: 3d3a5eeb7c58725834f8c6fd474c629787bf5f3a

--- fe/src/types/components/JobBadge.ts
+++ fe/src/types/components/JobBadge.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03401efea0278387b0186d7c97eee1380c723a6fb1ca714ae21b4f36de0b50d4
-size 76
+oid sha256:e9b81245d2f4f72b05f4fe165097c3c825554dd3b6a23813e9992c272d24b88b
+size 91
