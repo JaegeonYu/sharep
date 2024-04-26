8a1403b4b503fa35450602846e8a3887632465b7
김성제 <rlatjdwp0211@naver.com>
2024-04-26 13:30:24 +0900

    [Rename] InputWithLabel 타입 파일 이름 변경
    - input.ts -> InputWithLabel.ts
    
    
    Former-commit-id: 2390c230812bc6ec1c9bf25c8473aa44277d0e60

--- fe/src/types/index.ts
+++ fe/src/types/index.ts
@@ -1,3 +1,4 @@
-import { BaseInputProps, BaseLabelProps, BaseLabelWithInputProps } from './components/input';
+import { BaseInputProps, BaseLabelProps, BaseLabelWithInputProps } from './components/InputWithLabel';
+import { StatusBadgeProps } from './components/StatusBadge';
 
-export type { BaseInputProps, BaseLabelProps, BaseLabelWithInputProps };
+export type { BaseInputProps, BaseLabelProps, BaseLabelWithInputProps, StatusBadgeProps };
