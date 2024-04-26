98543e9c177957733ce255c30f94904b328793a3
조성규 <jo41010@naver.com>
2024-04-26 14:25:01 +0900

    [style] siderbar hover & svgs
    Former-commit-id: 151ef2b8f2db040859f27ee379d0663ff01e9bc9

--- fe/src/pages/SungGu/SungGu.tsx
+++ fe/src/pages/SungGu/SungGu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de75e7b070bc16dfb7926088773eca4d59e2cc10f224dea3a65008aabce6d1c9
-size 94
+oid sha256:d8cc50e39e26e663c69b102777b0917a14a5e3efca2691085cd8409c780c711b
+size 326
---
+++ fe/src/svg.d.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e71791e847136b69bd8a8bd281871e13af333b4c370dc55d683b67241119df29
+size 108
