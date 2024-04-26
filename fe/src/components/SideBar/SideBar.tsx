c0b1c44e6a588e38a3e107e37fb33fe253bb7c41
조성규 <jo41010@naver.com>
2024-04-26 14:42:46 +0900

    [feat] routing dummy pages
    Former-commit-id: 6a0f9418bc62799ba53566aa33078a34bd08648e

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:785f20ec4bbeffddad421fb96751e0ff6cf3b8a237194ad0f75ca8c94efe59e4
-size 3954
+oid sha256:017ee5704101acd799f4ac40cc014f758f7083b10b81a6631f235622bd7abea2
+size 4632
