a2ab78f45a0c9acd2f51398cc69a8de546adbef3
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-04-29 09:28:50 +0900

    [Add] Icon 추가
    - ManualTable에서 사용하는 아이콘 추가
    
    
    Former-commit-id: 2d03a2800ab06b1254db914bb7d5be25ce97f322

--- fe/src/assets/index.ts
+++ fe/src/assets/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01df026ae05348f6278e7c9b1f7070bd3936c801b069497e26989fb3aa13badc
-size 522
+oid sha256:5966045f89bfeed1fdec42b4b99c7d5e4ee9d54285999c3ed43fbc0446977a93
+size 771
