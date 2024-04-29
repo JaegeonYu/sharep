6612c940f846e9b3f3a767155b2c5432000ecf79
유재건 <yjk9805@naver.com>
2024-04-29 20:13:22 +0900

    [Feat] Project 생성시 리더 역할 생성 로직 추가
    Former-commit-id: e2d97f2f796e8fb737f0a69c2d596c2b90b8ef25

--- be/src/main/java/com/sharep/be/modules/project/dto/ProjectDto.java
+++ be/src/main/java/com/sharep/be/modules/project/dto/ProjectDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:910e718f936a0f746587769f9bfb5022f7960c2b666a4440fea3cefc450d091a
-size 1101
+oid sha256:f8f4d95999007d5a68a838155c8956fa7b49d2224b2f61d49ed2f08b5b2ca5b9
+size 1254
