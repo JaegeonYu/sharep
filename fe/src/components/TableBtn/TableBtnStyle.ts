09b428d80d230308bcd00b466cf1722ec81c5735
조성규 <jo41010@naver.com>
2024-04-29 10:07:30 +0900

    [Style] tableadd button complete
    Former-commit-id: 02bf20797b83eb9958b6a38e4911b6ea5d4a4db2

--- fe/src/components/TableBtn/TableBtnStyle.ts
+++ fe/src/components/TableBtn/TableBtnStyle.ts
@@ -3,8 +3,14 @@ import * as G from '@/styles';
 
 export const Wrapper = styled.div`
   width: 100;
-  background-color: blue;
-  /* border: solid 1px ${G.PALETTE.LIGHT_BLACK}; */
-
-  border-radius: 40px;
+  /* height: 42px; */
+  padding: 8px 0px;
+  background-color: white;
+  border: solid 1px #d0d7de;
+  column-gap: 10px;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${G.PALETTE.LIGHT_BLACK};
 `;
