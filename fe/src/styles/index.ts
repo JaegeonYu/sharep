76f72656d719b69d20086dc994cac934d04d6558
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-04-29 09:34:02 +0900

    [Chore] eslint와 PALETTE 수정
    - eslint에 extends 요소 하나 삭제
    - PALETTE에 색상 추가
    
    
    Former-commit-id: 1d4d20dcec8968ad43f7da243767abefaf18e029

--- fe/src/styles/index.ts
+++ fe/src/styles/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bf5f2c168e76a1f8d2840a2d73cf60e83caa5ebc95c18961139f81e4286b9c8
-size 2682
+oid sha256:5aa2b13e665c3f7d3275c5f4248f263d82393f5b8d0f6fd51956bea8342708bd
+size 2765
