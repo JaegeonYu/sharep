aa10fdf9e0f81d84dabc294a0bbf0184f2fa8e2f
임서정 <sjytis14@naver.com>
2024-04-29 15:32:03 +0900

    [Rename] Text 컴포넌트 StyledText로 변경
    Former-commit-id: d8f7dc5dd3f918aceabd50c37301ca55b963b8c4

--- fe/src/components/Commit/CommitStyle.ts
+++ fe/src/components/Commit/CommitStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a22336f5a71f3230d8245aca5aa61605bbe01870c7238eb4832bfa10e6b27a5f
-size 2248
+oid sha256:0180731a4557caafcc491df47bd0fb823a92b2f5379097f5ec6eb83682278f7d
+size 2254
