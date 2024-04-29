e235fb343e39123f9fb7aecdf79e0befaec939f0
임서정 <sjytis14@naver.com>
2024-04-29 14:36:49 +0900

    [Feat] 새 프로젝트 생성 모달 구현 중
    Former-commit-id: 18500fb6e2db4f3b2407e4bc323bb7864ffbb062

--- fe/src/components/Modal/ModalStyle.ts
+++ fe/src/components/Modal/ModalStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ea02a85a9d9c3b494f62f0fed84463604f03ff6f2eea86a4f3ead84ce60de98
-size 3245
+oid sha256:d68f3d137fd38ebfc34d6b8a646e37babaf9216d829703588fa9605aac2ddf06
+size 3041
--- fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationFormStyle.ts
+++ fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationFormStyle.ts
@@ -1,17 +1,3 @@
-import styled from 'styled-components';
-
-export const CustomInput = styled.div`
-  & > label {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 10px;
-    border: 1px solid red;
-  }
-  input {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    border: 1px solid black;
-  }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:23f79c889b1b7f101e1d6bd4badb067d535c5c4b30158b4f6ddbcfc288670ac8
+size 2691
