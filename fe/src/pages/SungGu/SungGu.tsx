79a7d1393de690c81fe50383bcc46f91bea7e470
김성제 <rlatjdwp0211@naver.com>
2024-04-29 15:06:45 +0900

    [Fix] build fail
    - lint 설정 또는 style props 전달 시 오류로 인한 build 에러
    
    
    Former-commit-id: dc069251b98fd509d82fc0c0b478d8310a580d51

--- fe/src/pages/SungGu/SungGu.tsx
+++ fe/src/pages/SungGu/SungGu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b3369eea71b3fa46a5e10c48102d21043f7502a030aa6322a7c03c50809f7e7
-size 684
+oid sha256:de96af6adbe86308a8fee63de892363efd89a4e3fe1d57cac94020f2d6c31bb0
+size 630
