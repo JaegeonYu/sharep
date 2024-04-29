b6df0d474676b6cbf46b42e692f6642fc9558f26
김성제 <rlatjdwp0211@naver.com>
2024-04-29 16:12:51 +0900

    [Chore] import alias 추가
    - tsconfig와 vite.config에 @layout import alias 추가
    
    
    Former-commit-id: e8ea9dc8d2fdef0f5102ebf05b4da773eb06d13a

--- fe/src/components/ManualTable/ManualTableStyle.ts
+++ fe/src/components/ManualTable/ManualTableStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:559fa3a8365d1431a45a1c489cfa1149acb03e30a94e04e5a67334b63eb0d933
-size 981
+oid sha256:3a21e0954ca47a34605092f23e08ba494a4b50c48e9677f58fe50b9126d270f0
+size 1001
