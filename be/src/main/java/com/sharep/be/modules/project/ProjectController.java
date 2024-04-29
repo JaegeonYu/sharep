727495a6fbe011278f9c2e1017d3738e66dd5651
jack <yjk9805@naver.com>
2024-04-29 10:41:58 +0900

    [Refactor] ProjectController 내 repository 의존성 제거
    Former-commit-id: 372e964f45e32beee80eeaf356e2cd1d5c3d6666

--- be/src/main/java/com/sharep/be/modules/project/ProjectController.java
+++ be/src/main/java/com/sharep/be/modules/project/ProjectController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51d232b0e1ad8a3ad3a931db153f03a63d1d19d3d387a0585a2bfe5f244a4e43
-size 3217
+oid sha256:33473b9e7fd39a13e1114756ab35db4428e6524d85c68a76fe3ba5921d9f28f0
+size 3106
