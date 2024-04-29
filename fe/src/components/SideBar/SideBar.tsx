9d7920afd6faf1590ff164c73195ef9183aadb26
김성제 <rlatjdwp0211@naver.com>
2024-04-29 16:12:02 +0900

    [Feat] Page Layout 생성
    - 생성 후 export
    
    
    Former-commit-id: 1ce2ce5ffd952de12c54da3dc9975eb0f280ef2e

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff6af7c9242c8f6335664e58a6a3688c2c24018373b9e79e6632ad541962654d
-size 4756
+oid sha256:ef7e464149e252006a3ddfe90bc9f288d78c78c8c894a5a8b682a6ae5093106b
+size 4785
