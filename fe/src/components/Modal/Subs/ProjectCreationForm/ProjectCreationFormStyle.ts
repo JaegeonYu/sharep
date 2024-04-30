30f605df17399554db3fa4cb14df46746f169a23
seojeong <sjytis14@naver.com>
2024-05-01 00:27:43 +0900

    [Feat] 새 프로젝트 생성 모달 직무 선택 버튼 추가, 팀원 목록에 팀장 default 배치
    Former-commit-id: b1e996fe6dbced4ec0ce490e1422258ddd2ca26c

--- fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationFormStyle.ts
+++ fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationFormStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:962d8b80fbe507a9e3d67cced984c90e142a9bb197f8687771fb230c7e02ccb2
-size 3601
+oid sha256:fafd763cc74983e5a6d31c5dd62a6441cb975e8da73da88c622cfceee3c1c7e9
+size 3997
