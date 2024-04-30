d61dc91f2ecec834a80de872aeb42c0669c8c756
조성규 <jo41010@naver.com>
2024-04-30 14:14:12 +0900

    [Fix] login pw masking
    Former-commit-id: 905c9a3841ccebc4cba9880178b242212a202f17

--- fe/src/apis/instance/index.ts
+++ fe/src/apis/instance/index.ts
@@ -4,7 +4,7 @@ export const instance = axios.create({
   baseURL: import.meta.env.VITE_END_POINT,
 
   headers: {
-    Authorization: `Bearer ${localStorage.getItem('')}`,
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
     // Origin: 'http://localhost:5173',
   },
 });
--- fe/src/pages/SeoJeong/SeoJeong.tsx
+++ fe/src/pages/SeoJeong/SeoJeong.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61ace2c189605463dfcad193df0fa4c8d611b4d2aa3ba1879c8dbfdfa398e7bc
+oid sha256:c7d479133fb618dc68cc6eb85eff19ea092cec1a6ec948ce1b94bde2d61950aa
 size 685
