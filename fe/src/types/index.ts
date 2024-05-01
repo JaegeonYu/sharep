0ee42434098c6db3ebc3abe3a613577d2b644309
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-01 19:55:45 +0900

    [Refactor] TeamDashboard type
    - TeamDashboard에서 사용할 타입들 types에 생성
    
    
    Former-commit-id: e9a0cda46abe44afae0745ddff1629ac3c15f328

--- fe/src/types/index.ts
+++ fe/src/types/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfd1c3ba24697fe4f5271da5aa91946d4f3cd85bbbe60891f68acb28d2076582
-size 1086
+oid sha256:b3e572be86824ebe8b22d9050f15f7a09cbb8c65923c8c2e8092e1b1e5d56813
+size 1410
