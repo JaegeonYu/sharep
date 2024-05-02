0c83e48a71edc9d76411fd70b440672afe626283
조성규 <jo41010@naver.com>
2024-05-02 14:45:58 +0900

    [Style] Grass Year layout
    Former-commit-id: cb1bc9274d31ecef3885246bcdb0eed100f336f8

--- fe/src/components/Grass/Grass.tsx
+++ fe/src/components/Grass/Grass.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1538040bc74f3222c696b5b1f8bc54a9465632eb12f0c8f3b920b3ff4640d96
-size 2414
+oid sha256:af01c196f7f11f0f43d303387ef42d6fb5087aeb38f54ff8b797e3f8f855737d
+size 2149
---
+++ fe/src/components/Grass/GrassStyle.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:92021e61d6e382f03efede01dd68c132d10081e7c3c4a1c1ad6ae0483696be94
+size 310
