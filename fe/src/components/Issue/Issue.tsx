37a3a74feecfc88567376c496f60835769700b8a
김성제 <rlatjdwp0211@naver.com>
2024-05-02 09:26:46 +0900

    [Remove] momentjs 삭제
    - dayjs로 시간 관련 작업할 예정
    
    
    Former-commit-id: 1579d86fe82a815ea42f9334a0a376c138d3c1d4

--- fe/src/components/Issue/Issue.tsx
+++ fe/src/components/Issue/Issue.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c15e491693951a8bf79df0de81bf262a2b3b2e7392ca60cae383291986d110d2
-size 2293
+oid sha256:982887a8a73f3fe1b170333ae213b3d68edbc638800fde6e0fa0fabbdd854558
+size 2299
