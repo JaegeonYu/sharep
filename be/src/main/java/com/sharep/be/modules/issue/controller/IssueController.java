196cc83ead8d64db6eaa0bc46aea2c73325d09ac
오상훈 <ph38230119@gmail.com>
2024-05-02 23:10:40 +0900

    [Fix] URL 복수형으로 수정
    Former-commit-id: f60e7574c0226369548a79c05a608d594ce3806f

--- be/src/main/java/com/sharep/be/modules/issue/controller/IssueController.java
+++ be/src/main/java/com/sharep/be/modules/issue/controller/IssueController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c58425b809c199344e2f882394148c8053aa20af463f2f6ca2380a3718be82c
-size 2877
+oid sha256:4f330fbfe0b09211e6a495f2093ae0e2a13145ad0305edd356ad54dd0760307e
+size 2949
