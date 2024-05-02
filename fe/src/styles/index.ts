38fe63c66407bf414bb00b689ed072b80834bac3
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-02 09:20:15 +0900

    [Refactor] 글로벌 스타일 수정
    - y scroll bar 두개 생기는 이슈 해결
    
    
    Former-commit-id: c9f109218c2632e6aad80706469a4d43ece36217

--- fe/src/styles/index.ts
+++ fe/src/styles/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4fbca41301761394f41ad7743105e5e27f9ee8c0774d58c017305a47cd87c79
-size 2790
+oid sha256:65a2a60314a7afd2be853566cc1db1fd29f02cef61e56b1b91ec0e8fdebd8aa6
+size 2821
