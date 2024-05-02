98ff8cb261faefbfdfb99dd53e4273627b291e2b
조성규 <jo41010@naver.com>
2024-05-02 15:14:31 +0900

    [Fix] delete unused var
    Former-commit-id: 0edc5b3f377bdd645715cbd47e32f059a50dd5d3

--- fe/src/components/Grass/GrassStyle.ts
+++ fe/src/components/Grass/GrassStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92021e61d6e382f03efede01dd68c132d10081e7c3c4a1c1ad6ae0483696be94
-size 310
+oid sha256:eeb8ae62983985f294edefefd9cb3c2af365609e4155f63dad92bd984a2cd4d9
+size 423
