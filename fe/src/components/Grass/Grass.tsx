3d1195a3de7c0bfce938b77f4aa0e1d6b3d4e875
조성규 <jo41010@naver.com>
2024-05-02 17:53:53 +0900

    [Fix] Grass UI fixed
    Former-commit-id: 5bef1646d7815a134c963dc3272199e92bdbc3a2

--- fe/src/components/Grass/Grass.tsx
+++ fe/src/components/Grass/Grass.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf952205aa6152b499de3c75bd1c74b7aa3b316b17ba8a340c1ad8a8c432b7fa
-size 1314
+oid sha256:2fd67f435651c837f31bf2e5a5ff7f3ca0710cacd53c6d5fccbdfada231c16c9
+size 1634
--- fe/src/components/ProjectGridWrapper/ProjectGridWrapper.tsx
+++ fe/src/components/ProjectGridWrapper/ProjectGridWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:989bce8a0e920494b34656e18f2bd78eb6526347e71cdffa829b76b1584e6c3f
-size 876
+oid sha256:dfe588a579ef979563ab3944ff6df169b83d1dc690f7046236462b6d596c34f2
+size 868
