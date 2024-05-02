c1ca2b75d56b349974a7a25288ca8e8b9c9f4e6f
임서정 <sjytis14@naver.com>
2024-05-02 17:56:17 +0900

    [Feat] modalData의 formData -> contents로 수정
    Former-commit-id: 3a644596a12d654581c4fc543e84c360b4dbded8

--- fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationForm.tsx
+++ fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f32c204bbdab7a1ea65cd1af5343929658627ab04dc14be579ca9fe617c247a8
+oid sha256:91d11372890bf1f186d13279dd700d39b846ce53674c92602904a37b2ea0b568
 size 11398
--- fe/src/pages/CommitHistory/CommitHistory.tsx
+++ fe/src/pages/CommitHistory/CommitHistory.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a43211cdefd7fe1e02c4e0e9480a07097b90f4f3ff5a615c4807481edcc96bb
-size 6528
+oid sha256:fd6591b22df4169fb3ef25a0158f7087fbd7d663a2582fe481b647747fbcdf96
+size 6531
