1fc1ed6b1dd03e1942bd9184530212c00260401f
오상훈 <ph38230119@gmail.com>
2024-05-02 23:23:50 +0900

    [Refactor] 필요없는 DTO 삭제
    Former-commit-id: 5ad5f80c9e27853dfdee512d0ce0f3002f94c5f0

--- be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepository.java
+++ be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepository.java
@@ -1,12 +1,11 @@
 package com.sharep.be.modules.issue.repository;
 
-import com.sharep.be.modules.issue.dto.FeatureDto;
+import com.sharep.be.modules.issue.Issue;
 import com.sharep.be.modules.issue.type.IssueType;
 import java.util.List;
-import java.util.Optional;
 
 public interface CustomIssueRepository {
 
-    Optional<List<FeatureDto>> findFeatureIssuesByProjectIdAndIssueType(Long projectId,
-            IssueType issueType);
+    List<Issue> findIssuesByMemberIdAndIssueType(Long memberId, IssueType issueType);
+
 }
