288e802414811db5570cc015b6068e9f7dbaeacf
seojeong <sjytis14@naver.com>
2024-05-02 23:35:41 +0900

    [Feat] 인프라 명세서 작업 추가 모달에 알림 부분 추가
    Former-commit-id: 97aae8f95667ed92aa299157f8febde28300062c

--- fe/src/components/Modal/Subs/TaskCreationForm/TaskCreationFormStyle.ts
+++ fe/src/components/Modal/Subs/TaskCreationForm/TaskCreationFormStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7a663718094809ec5b32d14a697edbfe4c5b84887a2a3b4be1458ae110c046b
-size 2773
+oid sha256:a7cddfcb8b3b23b4fcb3bfb7e6f2e9044ee90b5c5a5335c4329e7a6e7afcdf5d
+size 2774
