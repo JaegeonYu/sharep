f124c09e0898e847900822cfc90a1b25b46af0eb
김성제 <rlatjdwp0211@naver.com>
2024-05-02 09:26:46 +0900

    [Remove] momentjs 삭제
    - dayjs로 시간 관련 작업할 예정
    
    
    Former-commit-id: 9957923b363b764d3f9e34cd2f79af861557e4fb

--- fe/src/apis/instance/index.ts
+++ fe/src/apis/instance/index.ts
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c0459276492a597e2056616af8c445ef77392d474fe06a32b6d1038bfb770c39
-size 189
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_END_POINT,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('')}`,
+  },
+});
