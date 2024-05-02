ba689c9aa8f6e25da9e539f60321f82a042e52a4
seojeong <sjytis14@naver.com>
2024-05-02 23:35:41 +0900

    [Feat] 인프라 명세서 작업 추가 모달에 알림 부분 추가
    Former-commit-id: 319dfe32d7923e581a18feba3fb875ac31120bba

--- fe/src/components/Commit/Commit.tsx
+++ fe/src/components/Commit/Commit.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fcf57c982b059b86bf1c62f79cf08795f598befc1e1c7d5edaf4dc869fe0e5e
-size 2640
+oid sha256:0506a6bf70bc61042dc7d33407979d4df51e91e16ac26224032f95066446ad8c
+size 2624
--- fe/src/types/components/Commit.ts
+++ fe/src/types/components/Commit.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39d3dd9ff675cfb976fa0610db35512bf5fd3db0dac6fff30aa34afe90f09745
-size 290
+oid sha256:c77c5cdad40202c6261a01659abcebbb0af402d602e196e06736845890a250c7
+size 291
