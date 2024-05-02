e92c0b9028e00129a60069624200def70720a7f3
임서정 <sjytis14@naver.com>
2024-05-02 09:47:47 +0900

    [!HOTFIX] build fail error 수정
    Former-commit-id: 24d8b35a5661d2ed25d8573cb941c1b660a8ebb6

--- fe/src/components/Modal/Modal.tsx
+++ fe/src/components/Modal/Modal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9364ab1a2d1cd546eee8d3e43dbfbe4d87220b53eacee8133df943f8651f2e4
-size 2084
+oid sha256:8d2b1f10e846a71c434153e3e34d96c1a0b41d9de5b5e71c7f5faa4312bc9e26
+size 2106
--- fe/src/pages/Register/Register.tsx
+++ fe/src/pages/Register/Register.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3d91b3a4e64288d31c9d1034299200aa3fc65b48be622aa1a1c44795d05bdf2
-size 8831
+oid sha256:72fd82a93118bb3708de56646385a86bf27850a0771ac2bf0a8d7cc20cdda0ef
+size 8815
