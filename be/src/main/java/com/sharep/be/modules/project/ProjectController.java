7c311b0b6a2379c9477edcd93f375492cac52480
jack <yjk9805@naver.com>
2024-05-02 15:20:54 +0900

    [Feat] account imageurl length 추가
    Former-commit-id: de22efc96b6ec0683d523571bd95eccd41addc6d

--- be/src/main/java/com/sharep/be/modules/project/ProjectController.java
+++ be/src/main/java/com/sharep/be/modules/project/ProjectController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4577fa3efe9e5b496b9f478c7408b95c1f16fbfa74287f3e8134bac389162014
-size 3624
+oid sha256:9eadec622f7e92b0933c25c626476ef6ccc90078ab91364b808eeff162abe9a4
+size 3649
