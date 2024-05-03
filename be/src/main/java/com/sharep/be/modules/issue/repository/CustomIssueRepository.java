26d058a7c8fe10dfb643f733370b1a09c6805be7
오상훈 <ph38230119@gmail.com>
2024-05-04 02:31:48 +0900

    [Feat] 기능 명세서 조회
    이슈 삭제 로직 일부 수정
    Service 레이어에서 Entity 반환
    
    
    Former-commit-id: b956772564e30f750aa956fabc4a97a9a6124952

--- be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepository.java
+++ be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b947177a9505bc22d8d14b8df16281c1c9f6b49a7be1d532e9d73bb53650b9b3
-size 297
+oid sha256:6b7467715406cbe4ff96706c1dd55471b15afc934d1215e58a834b156712dbea
+size 385
