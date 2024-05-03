754c0093ea687bf41aa9d99393ad0b5b60164f62
김성제 <rlatjdwp0211@naver.com>
2024-05-03 17:31:21 +0900

    [Refactor] 변수명 또는 파일명 수정
    - job -> role
    - constrants -> constants
    - Canvan -> Kanban
    
    
    Former-commit-id: ba8e2ca788d5f2493a77a4bd01c6aa688d21bee8

--- fe/src/components/Commit/CommitStyle.ts
+++ fe/src/components/Commit/CommitStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0180731a4557caafcc491df47bd0fb823a92b2f5379097f5ec6eb83682278f7d
-size 2254
+oid sha256:0cd619103d9b96a5ae2a3c90c6e0da3f1428d157ebec75e8fc626a42c9e8f74a
+size 2255
--- fe/src/types/pages/TeamDashboard.ts
+++ fe/src/types/pages/TeamDashboard.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd13f76369ff4312d2fc61ec6d4c399bbf6b723c70fdb040fcb3b392cfdcaaee
-size 237
+oid sha256:36ccb7200fe410c01bd61d9a9a8bac362aa9c2bb35300d6e6549ed4a32ec5c3b
+size 240
