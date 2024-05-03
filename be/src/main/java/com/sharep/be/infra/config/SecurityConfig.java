eff3f8e4d55b09de95273354ef8fe1e979f250b9
jack <yjk9805@naver.com>
2024-05-03 10:02:04 +0900

    [Fix] deprecated 의존성 제거
    Former-commit-id: dbdd0d9f5c6550f2ef1182df5d31586476080d79

--- be/src/main/java/com/sharep/be/infra/config/SecurityConfig.java
+++ be/src/main/java/com/sharep/be/infra/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:870a3d46a7fb4a4e6066ae6fb4a312739fa7719c94ca6cfc219e84b13becc6f1
-size 6808
+oid sha256:b09e8645fbe2ca579167e3a788516c1ebfd4dd700440db851a2d825a16295013
+size 6290
