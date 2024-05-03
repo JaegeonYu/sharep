6dc6b655b80d80eb20069ae7ab680e1979164b93
seojeong <sjytis14@naver.com>
2024-05-04 02:23:38 +0900

    [Feat] 인프라 명세서 새 작업 추가 모달 알림 받는 유저 삭제 기능 구현
    Former-commit-id: fd1c4d37b5014cf36cf5b4b1637a24e8d004643c

--- fe/src/components/Modal/Subs/InfraTaskCreationForm/InfraTaskCreationFormStyle.ts
+++ fe/src/components/Modal/Subs/InfraTaskCreationForm/InfraTaskCreationFormStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc8674437bca25220b7533fd2ebd2947bbfaaab29252a2776cdd4b25a5a62497
-size 3709
+oid sha256:8243d3f4373c5e0581be58563a32c6f6c59b777c39be1e1536058516b3164852
+size 3708
