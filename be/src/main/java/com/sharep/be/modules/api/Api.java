6078b7e116282bd0947b245d6ad9c4c29137830d
오상훈 <ph38230119@gmail.com>
2024-05-04 02:11:16 +0900

    [Fix] 필요없는 IssueState 삭제
    Assignee에 이미 State가 존재함
    
    
    Former-commit-id: 43f355e33b3d8d82498949fe68ce0af9e3cf1de8

--- be/src/main/java/com/sharep/be/modules/api/Api.java
+++ be/src/main/java/com/sharep/be/modules/api/Api.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb73ea3526b67017caaa27c95a580cb125f79eecd948cba1f0da0d8d71caac97
-size 1678
+oid sha256:63d760ee2dd38317d33cb36fc41a368c246f9e82fbebaa222545916791c803c9
+size 1172
