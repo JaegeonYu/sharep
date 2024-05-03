e2e05fcba0c510f15c1c80ee3b61a8c2b431d415
임서정 <sjytis14@naver.com>
2024-05-03 16:10:15 +0900

    [Feat] 인프라 명세서 새 작업 추가 모달 구현 완료
    Former-commit-id: 26ab272cf00cd9415a66c0456093ab00a8fc1439

--- fe/src/components/Modal/Subs/InfraTaskCreationForm/InfraTaskCreationFormStyle.ts
+++ fe/src/components/Modal/Subs/InfraTaskCreationForm/InfraTaskCreationFormStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f1b8c94a06db84cdf28cc20a1d3a66131c2389442b214744a320e8e42909311
-size 2589
+oid sha256:bc8674437bca25220b7533fd2ebd2947bbfaaab29252a2776cdd4b25a5a62497
+size 3709
