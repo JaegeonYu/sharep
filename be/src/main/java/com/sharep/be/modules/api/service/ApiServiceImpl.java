e1ab8497f4e1c776b227bc2b415c626974ee0cf4
오상훈 <ph38230119@gmail.com>
2024-05-04 18:55:05 +0900

    [Fix] API 생성, 삭제 로직 수정
    API 생성이 안 되는 문제 수정
    Dirty Checking을 이용
    Issue의 상태 계산 로직을 Issue에서 처리
    
    
    Former-commit-id: 50d0c90cf14375a34c4a38eeab3194fdaa954abf

--- be/src/main/java/com/sharep/be/modules/api/service/ApiServiceImpl.java
+++ be/src/main/java/com/sharep/be/modules/api/service/ApiServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:704aad19f2c984fd5b02fd9a940ce8fc7c1da06c9b2eb6d863450cb1bb2c43fe
-size 1915
+oid sha256:625c145f43ec55ff329eda72d7e64506b9c6f31caa7b70fa6584ba58b2f2168e
+size 1950
