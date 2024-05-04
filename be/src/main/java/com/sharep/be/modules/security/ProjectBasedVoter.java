47be70cc27ccdbf161d189024f797c1ddbb08cef
유재건 <yjk9805@naver.com>
2024-05-04 16:21:37 +0900

    [Fix] ProejctBasedVoter Jwt 검증 로직 추가
    Former-commit-id: 08abaddae8e62696bc6e2749f85e51f8315ea810

--- be/src/main/java/com/sharep/be/modules/security/ProjectBasedVoter.java
+++ be/src/main/java/com/sharep/be/modules/security/ProjectBasedVoter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d2c09813e9c49346b53689718e653cd06e8b58b2d36d1539ce257766e089649
-size 2406
+oid sha256:837ef2414b7f63f91fecac59b673c1d9be4834bcff968175a115a1fedd531654
+size 2553
