49a8c50bcfdf8f4253906da23260fddbd6100c9c
seojeong <sjytis14@naver.com>
2024-05-05 01:04:14 +0900

    [Feat] 화면정의서 페이지 새로 만들기 버튼 추가
    Former-commit-id: c6347428f4d3d77318333992f5fb0cf152866686

--- fe/src/components/GalleryGridWrapper/GalleryGridWrapper.tsx
+++ fe/src/components/GalleryGridWrapper/GalleryGridWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e32da9f1cee689f400fe210817d331b73b6e53924e35c0a3875f10155c038a89
-size 749
+oid sha256:af3a8a39c11579d8c0cceb8e6cd8af834e73a9123665ed8cffc24273123de0e1
+size 1194
