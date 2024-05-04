ea6750c1bec3dabc112142689b75e788d3f60b80
오상훈 <ph38230119@gmail.com>
2024-05-04 16:29:27 +0900

    [Refactor] Dirty Checking 반영
    Api, Assignee, Job Response NPE 수정
    Assert 메서드 static import
    deleteApi 로직을 Dirty Checking을 이용하도록 수정
    
    
    Former-commit-id: 34351ce0b619b9077568872a1d187f0524facd14

--- be/src/main/java/com/sharep/be/modules/job/JobResponse.java
+++ be/src/main/java/com/sharep/be/modules/job/JobResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdcc8ae25016f9a9efd0dccea2d3fb6a8f3ad148001811014c75c56beba83fa6
-size 574
+oid sha256:6f7165d5d1317c16bc81ac44357482e2d89ae66bc56f128e6f364bcc7d0c7549
+size 697
