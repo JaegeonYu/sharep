96e1f764122804f5be8dbba019f2cf6e9f4888b5
seojeong <sjytis14@naver.com>
2024-05-04 17:07:37 +0900

    [Feat] Commit 컴포넌트 작업명+작업 내용 추가, props 변경
    Former-commit-id: cee24bae8211c8fa0d15128670f88fb93e0db152

--- fe/src/components/Commit/Commit.tsx
+++ fe/src/components/Commit/Commit.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0506a6bf70bc61042dc7d33407979d4df51e91e16ac26224032f95066446ad8c
-size 2624
+oid sha256:8828723d701dc9dde0ac96206d6f4bcef9f9aa19b151cacbf5ef8a03e4bf4bc1
+size 2810
--- fe/src/types/components/Commit.ts
+++ fe/src/types/components/Commit.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c77c5cdad40202c6261a01659abcebbb0af402d602e196e06736845890a250c7
-size 291
+oid sha256:19d837d02c4efd758de111d7943529beff83c2d9713752cbef99becd78c4f157
+size 363
