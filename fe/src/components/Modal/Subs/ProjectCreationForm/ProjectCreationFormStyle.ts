62cccc68df69efb8e83264e02f1a09bbdbf70b02
seojeong <sjytis14@naver.com>
2024-05-04 23:46:13 +0900

    [Refactor] Modal 중복되는 props 수정
    Former-commit-id: bd1751815eb6d4529a8c949f54d1f1162a105cc0

--- fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationFormStyle.ts
+++ fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationFormStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b645725f4e2a16f45dde1bf47b92a32cd45281be84601a4031a86332d8634ab2
-size 4035
+oid sha256:2539b987d3af716a2035a1704846ae3837cd3ed3ee32b6da24df78c854edbd65
+size 4128
