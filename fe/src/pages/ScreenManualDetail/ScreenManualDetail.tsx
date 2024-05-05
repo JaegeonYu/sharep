1f1ba9fd346443890ba3f1efc7df8d3e85e31d9a
seojeong <sjytis14@naver.com>
2024-05-05 16:54:22 +0900

    [Fix] screenManualDetail params 수정
    Former-commit-id: a9d29f44512cdcdfc50d4370dad95508c83bd522

--- fe/src/pages/ScreenManualDetail/ScreenManualDetail.tsx
+++ fe/src/pages/ScreenManualDetail/ScreenManualDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9b496930681632b021551d40b54705cf567d15149133c8efc3d5fec8d5a0bbd
-size 9428
+oid sha256:67eaf51735ae330f397fc9fe20429e2da6dfd69c12cbd921bb6eb7d1623b5625
+size 9442
