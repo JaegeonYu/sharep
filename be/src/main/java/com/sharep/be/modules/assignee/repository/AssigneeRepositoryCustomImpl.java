ef6ce96bcdee99fa51d53dba7dfdde2083f60543
Seungmin Lee <mehot98@gmail.com>
2024-05-05 16:03:31 +0900

    [Feat] 작업 조회 시 최신순으로 정렬
    Former-commit-id: 129242ef1742a7eab47a301711e54ea6cfabf69e

--- be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryCustomImpl.java
+++ be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryCustomImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16aa9cff5f1cd886168d7d31c15fc159e6f97d39ef24fc2fe90673cbc653c2a4
-size 1748
+oid sha256:25406e1779f98d344cb83481b110f17661ee18dd03fd79aa75f344d3be395861
+size 1846
--- be/src/main/java/com/sharep/be/modules/job/repository/JobRepositoryCustomImpl.java
+++ be/src/main/java/com/sharep/be/modules/job/repository/JobRepositoryCustomImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:066fc15b904d251958bdc27afa6141baf5a12bc178d6b455863c05325fe4a465
-size 3453
+oid sha256:3866b4b73ee72e02ce08db09859d67341fc33e6afd74820cc1ef4d402a48be71
+size 3641
