e15ec746f383ce1aea44b82812e7f73642b5d550
seojeong <sjytis14@naver.com>
2024-05-05 16:10:29 +0900

    [Feat] 화면 정의서 상세 조회
    Former-commit-id: 5a2cfbb856e81f7a392265202cc943a6e5fd3915

---
+++ fe/src/pages/ScreenManualDetail/ScreenManualDetail.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b90af72c393867d09f456f9c3b09f11bed0ccd5b60edad2c3cfadedf410c2b0a
+size 9471
--- fe/src/types/components/GalleryCard.ts
+++ fe/src/types/components/GalleryCard.ts
@@ -1,6 +1,3 @@
-export interface GalleryCardProps {
-  issueName: string;
-  createdAt: string;
-  issueType: 'SCREEN' | 'PRIVATE';
-  imageUrl?: string;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a7eb7f43a9189494effa6616f5349153a85589b04bff0f4717f1aba9b40f705
+size 155
