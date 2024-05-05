e040fe2cebaf7ec1759247929288850ce4e3f853
seojeong <sjytis14@naver.com>
2024-05-05 16:10:29 +0900

    [Feat] 화면 정의서 상세 조회
    Former-commit-id: 1a968ead46b280f5434792687955d157ec1aac6e

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:711ff336308773d897933125e64b1635f5c8c54d2c18a9e5364da1a7a5b5ca60
-size 1391
+oid sha256:718bdf0c17f33c50c77ffbe7d0e57f043b51792e12e06645e838823585d56ddb
+size 1501
---
+++ fe/src/pages/ScreenManualDetail/ScreenManualDetailStyle.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b2966b36cf7907db8254cd2d57d6edfcd0f759dc3ba593714e37b81c69866d2
+size 2294
