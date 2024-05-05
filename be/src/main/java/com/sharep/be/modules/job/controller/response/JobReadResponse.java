1437d144b8fc2c86315f08ca3cc2ec0ae35755ed
Seungmin Lee <mehot98@gmail.com>
2024-05-05 16:21:50 +0900

    [Feat] 작업 조회 시 멤버 반환값 수정
    Former-commit-id: fcfc3f23330396b673197279a8fec25dfe9add10

--- be/src/main/java/com/sharep/be/modules/job/controller/response/JobReadResponse.java
+++ be/src/main/java/com/sharep/be/modules/job/controller/response/JobReadResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdcc8cc8a74017095e3f695335a57b24d58c3507042b442f20148197047de22e
-size 696
+oid sha256:6f34daa0756ac116375885a5e5e5211feb756eeeb71b1ad354ec65fee3b6b3eb
+size 769
