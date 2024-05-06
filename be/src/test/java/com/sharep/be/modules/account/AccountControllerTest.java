e160ca46aeceb5c088944ab92684d4dc59434cd5
유재건 <yjk9805@naver.com>
2024-05-06 15:41:07 +0900

    [Fix] Assignee Test 주석처리
    Former-commit-id: 953ec22f9482a85dca35d3a53c4e61d12a9bdb40

--- be/src/test/java/com/sharep/be/modules/account/AccountControllerTest.java
+++ be/src/test/java/com/sharep/be/modules/account/AccountControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7259c46d3be21a9196153662f5374213af906e78e7cc127daef5919725bc42f
-size 2555
+oid sha256:6f864dcef3da10b8daee9bd2a480dba2bdbfc157978275124868ef7aeb8f5f24
+size 2509
