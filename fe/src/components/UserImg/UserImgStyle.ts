9fd105e5b5274318113774f2bf8bd724dd79cae8
조성규 <jo41010@naver.com>
2024-05-07 08:52:36 +0900

    [Fix] Edit Grass
    Former-commit-id: fb6f028cfb8c02fcbc274f3092ef7e039a498d43

--- fe/src/components/UserImg/UserImgStyle.ts
+++ fe/src/components/UserImg/UserImgStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56c34c8b13799116a95330ee871011bd24ea3a7e84a1674424a65b1b2b78d2a9
-size 529
+oid sha256:5db3accae9ec086d8b64d5d1cd97ef52565e85bd970a09503014794b590bcac2
+size 982
