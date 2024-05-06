8cd408b18f4b3c977bead95bb311f934a433d771
유재건 <yjk9805@naver.com>
2024-05-06 22:40:31 +0900

    [Feat] Member - Job 연과관관계 추가 , í어제 J한 Job + Member Join 쿼리 구현
    Former-commit-id: c46684d8b322e58e6c50c58f6c4a71a7baccbd10

--- be/src/main/java/com/sharep/be/BeApplication.java
+++ be/src/main/java/com/sharep/be/BeApplication.java
@@ -1,15 +1,3 @@
-package com.sharep.be;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@SpringBootApplication
-public class BeApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BeApplication.class, args);
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8faed07d0aa901e4d387cb12cdc48c35e6713b20b98f235286c752bb6cd82af
+size 487
