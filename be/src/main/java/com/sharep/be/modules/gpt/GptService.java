49a78c2f07650a68957eb829f2156d4d2ef57ca3
유재건 <yjk9805@naver.com>
2024-05-06 22:35:31 +0900

    [Feat] Gpt API 사용 구현
    Former-commit-id: af77f0db04987da5119839ef27c4da1a6fcc9c66

---
+++ be/src/main/java/com/sharep/be/modules/gpt/GptService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:77d590237fceaa6319fa2425312dfa35e9fc4de91a97a2070741a4095ae845cd
+size 3035
---
+++ be/src/main/java/com/sharep/be/modules/gpt/dto/GptResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:31e280aa91487025877ba735444e083ba8d5b86da59f91d1b5866a05eb45c65d
+size 1279
