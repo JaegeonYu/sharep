0ff6f5c85eb152e2bd58fb7cedd22b735ebeaf8a
이승민 <mehot98@gmail.com>
2024-05-07 12:11:07 +0900

    [Feat] 기여도 반환값 수정
    Former-commit-id: e2f0b7105a4964637cd30b0689f46cea35427873

--- be/src/main/java/com/sharep/be/modules/job/controller/JobController.java
+++ be/src/main/java/com/sharep/be/modules/job/controller/JobController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faad85181325d311509ec7f8301f63bd723e4f629a369a2320cc96baf838c22e
-size 3885
+oid sha256:6bad092ba2da4ca53c44af69263aa5db8ee1b4b347ee9f7b4c9e11321e220866
+size 3888
--- be/src/main/java/com/sharep/be/modules/job/controller/response/JobContributionResponse.java
+++ be/src/main/java/com/sharep/be/modules/job/controller/response/JobContributionResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e154293fa8944e6cbd34ff2aa60169f511b29b2d0dc1aabb4fa24127ff82790d
-size 647
+oid sha256:9cbe24e732a44df5c2721ff64d74fede9b587ab9165f55960a3170e2de40467c
+size 987
