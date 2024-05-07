e1b4a12891c8b6541e7fe30379ff04e7a329ea9f
조성규 <jo41010@naver.com>
2024-05-07 16:26:35 +0900

    [Fix] User Status Check
    Former-commit-id: 49b574c87afc505d2d0eef805c2fc5d1ccd10715

--- fe/src/pages/Mypage/Mypage.tsx
+++ fe/src/pages/Mypage/Mypage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:648ba4d33f7275335ac26fa1dc93cc330ae6a159d7a6b29ae8453e157a985e5b
-size 4644
+oid sha256:7c5314a97d21337b8f9922c7488257e770f53a9b26d4074f1bf1a9b50b230fce
+size 4675
--- fe/src/stores/atoms/loadUser.ts
+++ fe/src/stores/atoms/loadUser.ts
@@ -1,9 +1,10 @@
 import { atom } from 'recoil';
 
 interface User {
-  id: string;
-  username: string;
+  id: number;
   email: string;
+  nickname: string;
+  imageUrl: string | null;
   // 다른 필요한 사용자 정보 추가 가능
 }
 
