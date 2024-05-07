7ce3894a741764af8aac351214932ca54fa63ab3
오상훈 <ph38230119@gmail.com>
2024-05-07 23:58:40 +0900

    [Feat] 이슈 연결 구현
    기능 이슈와 화면 이슈를 연결하고 삭제 API 구현
    이슈를 연결한 후에는 연결 id를 응답하고, 연결을 삭제할 때 연결 id를 Path Variable에 넣어서 삭제
    
    
    Former-commit-id: 6db6ff87a378d0212c39e45d780e50ff4e275e27

---
+++ be/src/main/java/com/sharep/be/modules/storyboard/StoryboardRequest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:57d89d15c4fdfc0aeb073958ea406b960ab6643c473723e6e4ef5caf51a88cea
+size 571
---
+++ be/src/main/java/com/sharep/be/modules/storyboard/controller/StoryboardController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2ff079fc8f67ba82efa73d11a28785dc7f16d4aa12a70f393d4ed6f03334c8e2
+size 1600
