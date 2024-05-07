9bb8df1307cbc5495b1e6fa164ddb40d379293ff
Seungmin Lee <mehot98@gmail.com>
2024-05-07 21:16:46 +0900

    [Fix] 팀원 현재 이슈 구성원 반환값 변경
    Former-commit-id: fd0d035069f09bb18d2e88d689dfb09492fff24e

--- be/src/main/java/com/sharep/be/modules/issue/IssueNowResponse.java
+++ be/src/main/java/com/sharep/be/modules/issue/IssueNowResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:300d47a2350df66af60d23d5961750a071d78b1e5d8cb6870d0af126b0c5a8f2
-size 760
+oid sha256:550f288880f57566409047a20aca2f59c99bc4112050f766c2ffaa4328c1c70b
+size 861
