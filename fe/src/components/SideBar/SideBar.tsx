753eab6b5594cdd0082a7563d430ef9af7c3080e
조성규 <jo41010@naver.com>
2024-05-07 16:51:20 +0900

    [Fix] SideBar API Connect
    Former-commit-id: cec2a77ddab0742cda41289ecf166c3fb192a5e5

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75a2e94001273f6cb2d4516ae069e45ec646212f3327c09f10fe6456f508d56a
-size 8430
+oid sha256:1f9577cb4a3bcf7335d53cf48698be0dbbbafd0b57131e82764663eaa4711f2c
+size 8580
