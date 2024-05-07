f67dce4f058e3a3d10bf18e01a58e6fdc6a50d7f
seojeong <sjytis14@naver.com>
2024-05-08 00:39:56 +0900

    [Feat] 새 프로젝트 생성 api 연결
    Former-commit-id: c0125fbbeb2d1b8d7485a9ee34dba2a7ed9eb3db

--- fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationForm.tsx
+++ fe/src/components/Modal/Subs/ProjectCreationForm/ProjectCreationForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96424126248012fadb09c185b7eaac50940fba5adf046e29a323a4f06c1baed1
-size 9586
+oid sha256:ffa0bc6074fee6b81d0998d5ed580613110bf29bca88c6ad995ff51012d1434f
+size 10231
--- fe/src/pages/ScreenManual/ScreenManual.tsx
+++ fe/src/pages/ScreenManual/ScreenManual.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9d71d0436692356fc4cf67c26202c9f3daa871212a474ae725f0e311ecf80f1
-size 1397
+oid sha256:7619525847dd0e1c7d64f6a2d40db7d9cb470bab6c7f601c73fe05a086a47011
+size 1244
