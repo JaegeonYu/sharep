80746c61cdd64097d6542f62de2c25ae95208988
김성제 <rlatjdwp0211@naver.com>
2024-05-07 12:35:11 +0900

    [Feat] axios instance
    - Application/json 타입 인스턴스와 Multipart/form-data 타입 인스턴스로 분할
    
    
    Former-commit-id: 1fbf6ef1ca5736327060b2f828d788c51a6c556d

--- fe/src/apis/projects/index.ts
+++ fe/src/apis/projects/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb973f01be7f91018c6514f324db2cb84d54782b297fca85df6373479c7a7160
-size 836
+oid sha256:44509c60358088c8a70cd885f7993728f3fa3078223e1f7f37cd22edf8e5a16b
+size 1239
