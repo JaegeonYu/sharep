68d912b719b209f69ce42b8669dc347c8993595f
jack <yjk9805@naver.com>
2024-05-07 17:25:44 +0900

    [Feat] Contains 이메일로 계정 반환
    Former-commit-id: b13f8cc05ce14aa6aa853e5e898b37e281a23e35

--- be/src/main/java/com/sharep/be/modules/account/service/AccountService.java
+++ be/src/main/java/com/sharep/be/modules/account/service/AccountService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9abe161820311bb1fe5ec596b4671ed2d0e3ea4eb05794d8c56ccc68e50f461f
-size 2331
+oid sha256:22119a7b17a6c431e40c0f6a1e9799a46488e2ee2924b998152787270473dfb6
+size 2477
