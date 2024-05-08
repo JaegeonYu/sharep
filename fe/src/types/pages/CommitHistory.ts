6e817a883a2fd76ea4f117efb616303fb759216b
seojeong <sjytis14@naver.com>
2024-05-09 01:11:10 +0900

    [Feat] 작업 히스토리 필터 refetch 하도록 수정
    Former-commit-id: da0120670da430b3d1e1e253bfc1b879b02d9213

--- fe/src/types/pages/CommitHistory.ts
+++ fe/src/types/pages/CommitHistory.ts
@@ -5,9 +5,9 @@ export interface CommitHistoryProps extends Omit<T.CommitProps, 'disabled'> {}
 
 export interface FilterProps {
   type: {
-    member: { id: number; nickname: string } | null;
-    role: Extract<T.RoleBadgeProps, 'role'> | null;
-    issue: { id: number; issueName: string } | null;
+    accountId: number | null;
+    roleType: Extract<T.RoleBadgeProps, 'role'> | null;
+    issueId: number | null;
   };
   icon: React.JSX.Element;
   label: string;
