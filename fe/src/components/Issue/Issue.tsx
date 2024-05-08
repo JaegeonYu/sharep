32c75895ac0e913bc3ca51aa4eb6a96cca968544
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-08 09:25:21 +0900

    [Feat] API 추가 및 스타일 추가
    - delete issue 추가 및 해당 UI 추가
    
    
    Former-commit-id: 345bcfe4212236049aae9e09bd4c8bc6dfd54e19

--- fe/src/components/Issue/Issue.tsx
+++ fe/src/components/Issue/Issue.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6edaea19a75568a23d9c6c403637bbb1db2e5d87d8b4158b59d815a3620a5fa2
-size 1679
+oid sha256:9fa2974401ada3d16cc8ce75dccb6b891ee40a8d1d9c574d1e1c0b5f544bdf0a
+size 2705
