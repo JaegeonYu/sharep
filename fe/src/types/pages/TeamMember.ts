5faef2b57babea100dc19eae6f96ada5b4da659e
김성제 <rlatjdwp0211@naver.com>
2024-05-08 13:02:46 +0900

    [Feat] API response type 정의한 것 반영
    - src/types/apis에 정의한 것 반영
    
    
    Former-commit-id: bb3f66580b42376b06f6d9d153a7a7ce9e07ebf4

--- fe/src/types/pages/TeamMember.ts
+++ fe/src/types/pages/TeamMember.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69f819f1534989bc333e41b013e904c8c6a82b955adcc8e9500175b02583e57e
-size 959
+oid sha256:2f9238eb195182b0d398f26525607951175dcb06abb0e21ecabd5291c534abee
+size 943
