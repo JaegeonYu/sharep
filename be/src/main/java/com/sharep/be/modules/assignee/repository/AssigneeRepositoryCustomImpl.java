6367e8d380ccf9a008f66e1a7b13eb30ea1e8245
이승민 <mehot98@gmail.com>
2024-05-08 13:18:07 +0900

    [Refactor] 현재 진행중인 이슈 검색 쿼리 최적화 및 코드 개선
    Former-commit-id: fc19090cac24a28ce9f4ee12e48a0d4e4952519a

--- be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryCustomImpl.java
+++ be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryCustomImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3433d99b22ef7208a51fa1d0b12a5e1b96fc03e4240a85aafcea2c628e515571
-size 3791
+oid sha256:3ab2b7e0b714e8aeff7b6216d427a7109ab0d9376d057e2d588c92dea4aeab44
+size 3694
