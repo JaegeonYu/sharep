fa4f01833382b310ffda8ac97d0464597fc1eb81
jack <yjk9805@naver.com>
2024-05-08 15:06:58 +0900

    [Fix] Issue based Voter 오류 로직 수정
    Former-commit-id: e540fca716ac7a18e96e8a460c4a3af93869aecc

--- be/src/main/java/com/sharep/be/modules/security/IssueBasedVoter.java
+++ be/src/main/java/com/sharep/be/modules/security/IssueBasedVoter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67927cdb1ce4bab85a599bf56a50b54aaf262f38d325ce3000e5bd8427c7724c
-size 2751
+oid sha256:092bb2fe5e1888c16c2b851af4568bde2360c63810d984bce6164c38bafc7049
+size 2960
