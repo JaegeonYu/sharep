2a111dd37504a930a0ed704896679dd97dc71b29
오상훈 <ph38230119@gmail.com>
2024-05-08 09:45:04 +0900

    [Refactor] PUT, DELETE 응답 코드 수정
    OK -> No Content를 반환하도록 수정
    
    
    Former-commit-id: fca78a78b017954cb46d782132e4797378cfe3b4

--- be/src/main/java/com/sharep/be/modules/storyboard/controller/StoryboardController.java
+++ be/src/main/java/com/sharep/be/modules/storyboard/controller/StoryboardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ff079fc8f67ba82efa73d11a28785dc7f16d4aa12a70f393d4ed6f03334c8e2
-size 1600
+oid sha256:9e71bab42b2b1cba3d4fef2ca61f18828a6f6c1561ada45ae81d235556b988dc
+size 1607
