8d6aa8eb09a79c1b1366fdc8ac9091ee9771f0da
임서정 <sjytis14@naver.com>
2024-05-08 13:43:24 +0900

    Merge branch 'dev/fe' of https://lab.ssafy.com/s10-final/S10P31B207 into feat/fe/comp/modal
    Former-commit-id: 09a55db06a75ba7791054ebe755822369f733820

--- fe/src/apis/projects/index.ts
+++ fe/src/apis/projects/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd89b18d3cc3130ada6a929ce173ae5384f37e5cf16cc4baf97f4e5e713216b0
-size 4009
+oid sha256:cac22fa91809b5b315defa337a790240d30e0cf27352d945bec5228e178d3346
+size 4448
