8fc00e7d83287018c298ed4ad3e95dce720c4120
조성규 <jo41010@naver.com>
2024-05-09 16:00:43 +0900

    [Fix] login & regi btn
    Former-commit-id: 7d5d2e5edb26b23b197c7444f2dada1aa9e85de4

--- fe/src/components/Header/HeaderStyle.ts
+++ fe/src/components/Header/HeaderStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c935930b40343662ac76fd89932383953e3973e8fcb00cd06f82dd42a82fb404
-size 762
+oid sha256:0f856815f7b9c02c440cd93df9a6a7c16d08b6b0b01572488909b810de0fe9f8
+size 756
--- fe/src/pages/Login/LoginStyle.ts
+++ fe/src/pages/Login/LoginStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:789034c0e2fb273c80452edda8c373daeda57fa729f7eff08b409387f2f743ec
-size 2095
+oid sha256:c7e6cd906b04e236dbc4d90e4afe8069220daeb85d6389a2812af3632e95d164
+size 2089
