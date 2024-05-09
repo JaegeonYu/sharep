dc6044c02cf147d3e32fbe7015698a1f727d23a6
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-09 09:07:43 +0900

    [Feat] Textarea Cel
    - 입력 시 height 자동 조절
    
    
    Former-commit-id: 0133da3163d6073d067a73f5d80a59855a7b0d92

--- fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCel.tsx
+++ fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e988220a9a284883093ef87028789c6bcfc90806cdc31fba21ef9b504733c983
-size 1520
+oid sha256:c9986f1c92e326f701424f14bce891d4f835d7c82eb88fb0d1a669bf3932df76
+size 2032
--- fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCelStyle.ts
+++ fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCelStyle.ts
@@ -1,12 +1,3 @@
-import styled from 'styled-components';
-import { PALETTE } from '@/styles';
-
-export const TextAreaCel = styled.textarea<{ $fixedWidth: string; $isEditingMode: boolean }>`
-  max-width: 100%;
-  width: ${({ $fixedWidth }) => $fixedWidth};
-  min-width: fit-content;
-  min-height: fit-content;
-  border-radius: ${({ $isEditingMode }) => $isEditingMode && '6px'};
-  box-shadow: ${({ $isEditingMode }) => $isEditingMode && '2px 2px 16px rgba(0, 0, 0, 0.36)'};
-  color: ${PALETTE.TABLE_CONTENT};
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5d31495e9f167450edd4c65e9b9f31cfec54320c2260629e6fa23122cd9e3cb
+size 677
