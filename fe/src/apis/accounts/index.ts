3384345ca807e317153fcde4c4fa933f1aa50386
조성규 <jo41010@naver.com>
2024-05-09 09:09:43 +0900

    [Fix] account url fixed
    Former-commit-id: b5a21e3e4ebd89fa76517202e6287f933809ed4f

--- fe/src/apis/accounts/index.ts
+++ fe/src/apis/accounts/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee6772cb6239e44db12618fc1fbf9a33c079a72bc238ffd85c378f78b325f18b
-size 708
+oid sha256:0a79629eb4e3d63615c13ee2c14c9c609ef16a4a174799ff1931183a2d0fb92f
+size 714
