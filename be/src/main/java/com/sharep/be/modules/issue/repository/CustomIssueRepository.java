e4bea5c10d6a4c143f249f3ffdcee6a0a5996819
오상훈 <ph38230119@gmail.com>
2024-05-09 17:14:59 +0900

    [Feat] 단일 이슈 조회 쿼리 생성
    CrudRepository의 findById로 자세한 단일 이슈 정보를 쿼리할 때 발생하는 N+1문제를 쿼리를 구현하여 해결
    
    
    Former-commit-id: ce6961dd731f8ba5e94fc1b74ce6002c047e7b32

--- be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepository.java
+++ be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c72fcfb87c27ec2916d47cf638750f9b31bab80500a06e1168672bf6be58331d
-size 405
+oid sha256:58482310844dae4440074ae6b6b54e362969152be0a69190f916852a88d8ebec
+size 477
--- be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepositoryImpl.java
+++ be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25ddabc9be5d8892b4c8307c005b5169dbbb1fa1c470df294e46ea0802e5b547
-size 3261
+oid sha256:5c8797e63f64c6db1557764de9cefa1894e88f690d18c359f62c69d7f2ca8bff
+size 3808
