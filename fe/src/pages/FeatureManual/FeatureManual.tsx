82a6ccbd67f21a497b1ac119c9bd763d6abfe7dc
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-09 22:39:45 +0900

    [Feat] FeatureManual
    - Todo: 리스트 Get API 요청 후 반영
    
    
    Former-commit-id: 1c9f3768ffd06d3fed42b3edeeb1a0a5dabb9c01

--- fe/src/pages/FeatureManual/FeatureManual.tsx
+++ fe/src/pages/FeatureManual/FeatureManual.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2a075011d2845494ee5613c1de6fb3339ea4c70e1b6ecd08b5766749a771c18
-size 3031
+oid sha256:884243c279e33d5d218cde27cabb874aa9eaf16d95c242cbf47d449d697fbb19
+size 3637
