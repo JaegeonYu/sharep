e09e8ff9fd43981af998639257ebbddee7b4f4bf
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-10 04:28:45 +0900

    [Feat] 새 개인 이슈 생성 & 어제 한 일 요약
    - API 연동 및 UI 반영
    
    
    Former-commit-id: 783ed92df73fc9279c89f2d9d8241826d3ab27f0

---
+++ fe/src/pages/TeamMember/Subs/CreateIssue/CreateIssue.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70aff644b596d8dedc0dd6a5407416f59123d90a6c922f49dbae88bd2443803c
+size 2298
--- fe/src/pages/TeamMember/Subs/CreatePrivateIssue/CreatePrivateIssue.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import * as S from './CreatePrivateIssueStyle';
-
-export default function CreatePrivateIssue() {
-  return <S.Wrapper></S.Wrapper>;
-}
