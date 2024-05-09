572e6a5d92e39e9ed50a9dc2722570408418e2fa
seojeong <sjytis14@naver.com>
2024-05-10 00:00:01 +0900

    [Fix] galleryGridWrapper props simpleIssue type으로 수정
    Former-commit-id: 7a548d7bf67ad54309d945a962e6469904238d7c

--- fe/src/types/components/GalleryGridWrapper.ts
+++ fe/src/types/components/GalleryGridWrapper.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d20b4d6a3ebe00b82814d822c0d0270740aec1bfd4d0a65e5333421f2862b9cc
-size 176
+oid sha256:258b6e65d6c180a06841184ae3dcba3afcad739b31249012530c4eb774a13f18
+size 137
