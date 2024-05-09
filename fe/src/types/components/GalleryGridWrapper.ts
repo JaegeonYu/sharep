a9fda68c3f44d9415e8faac424662af659721d5a
seojeong <sjytis14@naver.com>
2024-05-09 23:17:52 +0900

    [Design] 작업 기록 버튼 색 변경
    Former-commit-id: 0a6154be6faf04864fbe5c541e43177991c89eec

--- fe/src/types/components/GalleryGridWrapper.ts
+++ fe/src/types/components/GalleryGridWrapper.ts
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1710b1b0feb93976ecb907326a637e9e78be0262267306f49aa1403e4ca112af
-size 146
+import { GalleryCardProps } from '..';
+
+export interface GalleryGridWrapperProps {
+  issueList: GalleryCardProps[];
+  type: 'SCREEN' | 'INFRA';
+}
