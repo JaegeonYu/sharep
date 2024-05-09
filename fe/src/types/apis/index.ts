25ae0e03b9e6b7c76ebbd3a7ee8421b251419213
임서정 <sjytis14@naver.com>
2024-05-09 17:51:40 +0900

    [!BREAKING CHANGE] issueListResponse props 수정
    Former-commit-id: dbb5e6fd05979ad3a0a8400f3118be76cd475678

--- fe/src/types/apis/index.ts
+++ fe/src/types/apis/index.ts
@@ -1,21 +1,3 @@
-import {
-  DetailIssue,
-  SimpleIssue,
-  GetContributionsResponse,
-  GetJobListResponse,
-  GetNowIssueListResponse,
-  GetProjectMemberListResponse,
-  GetProjectIssueListResponse,
-  //   GetScreenIssueListResponse,
-} from './projects';
-
-export type {
-  DetailIssue,
-  SimpleIssue,
-  GetContributionsResponse,
-  GetJobListResponse,
-  GetNowIssueListResponse,
-  GetProjectMemberListResponse,
-  GetProjectIssueListResponse,
-  //   GetScreenIssueListResponse,
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:63884e2901f82a79f0b3e108a07b813f733c55187ecbeb69e2934039b4203f23
+size 516
