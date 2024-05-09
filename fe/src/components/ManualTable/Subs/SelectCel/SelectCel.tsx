4bffb81a6341a1a00aabfeb9d14bd4ecc3bc3f67
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-09 22:44:40 +0900

    [Remove] TextCel -> SelectAssigneesCel
    - Todo : member list 받아 온 후 SelectAssigneesCel에 반영
    
    
    Former-commit-id: d3ba5c3141102d5d434b56010d5032f3c7ba19d9

--- fe/src/components/ManualTable/Subs/SelectCel/SelectCel.tsx
+++ fe/src/components/ManualTable/Subs/SelectCel/SelectCel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ec0a9c5b2906b4a60d600569eb01850522295481aa7bd8bb2d8bcd8ed26e011
-size 3396
+oid sha256:ac4ac643953345d1f8af77284b8d98351402c87ddddc0f21275fcfb31a2ba550
+size 2521
