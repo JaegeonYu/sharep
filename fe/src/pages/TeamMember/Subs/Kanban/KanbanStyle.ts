856295df353576d2427ef5da6157eecb792a554f
김성제 <rlatjdwp0211@naver.com>
2024-05-09 17:13:29 +0900

    [Refactor] 타입 수정 반영
    - 반영 및 디테일한 스타일 수정
    
    
    Former-commit-id: cc5f180b1405ec7cf21ff40cc502f2f4148b036b

--- fe/src/pages/TeamMember/Subs/Kanban/KanbanStyle.ts
+++ fe/src/pages/TeamMember/Subs/Kanban/KanbanStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c3c5d8fddf62bb9f4b8bf1cbfa72286d51f4000502d3382eecdd3042cfc00d0
-size 671
+oid sha256:4ee7f8ca2e05252714e6c66196fc0e26a8ab30d6c8dfc1cd545910b28efcaf5a
+size 693
