c5599bf3f4997ff1bb9df87dab1293df2995ec7f
seojeong <sjytis14@naver.com>
2024-05-10 02:24:42 +0900

    [Feat] 화면이슈에 담당자 생성 기능 추가
    Former-commit-id: 304ff34f7cf0049219977ff7e4f6a2f29229e37f

--- fe/src/pages/ScreenManualDetail/ScreenManualDetailStyle.ts
+++ fe/src/pages/ScreenManualDetail/ScreenManualDetailStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fb5018589061a566d965d4f24f307eecc9f821fd44a1a18eeb07dab6640851d
-size 2515
+oid sha256:2c2bebecd1ceed574be5bf793ac71b0fa55ee4a03c5d568009939ba44e366cfc
+size 4543
