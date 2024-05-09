77f2091fe36b96f9dd7c2ea12234860019fd2113
임서정 <sjytis14@naver.com>
2024-05-09 15:41:32 +0900

    [Refactor] 이슈 생성 mutation 수정
    Former-commit-id: 010b714b9dca8df42c90b17b56286aca500fd871

--- fe/src/components/GalleryGridWrapper/GalleryGridWrapper.tsx
+++ fe/src/components/GalleryGridWrapper/GalleryGridWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e51f6177090229426adee169b9b7cb1d5a04b64e44490e2bac76b5278d619764
-size 2928
+oid sha256:2bbed56c6cce3fde2167eae4eae3e662cf83632af675c03e10a551b3ac07dd1c
+size 3007
