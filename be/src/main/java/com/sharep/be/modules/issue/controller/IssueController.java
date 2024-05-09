eb94278fa9ef335077eee0a3aaea5dcbffb90714
오상훈 <ph38230119@gmail.com>
2024-05-09 17:11:48 +0900

    [Refactor] 이슈 반환 레코드명 변경
    ConnectedIssueResponse -> SimpleIssueResponse
    
    
    Former-commit-id: 8c247f3f950100bee0282c7d0154340c847443ba

--- be/src/main/java/com/sharep/be/modules/issue/controller/IssueController.java
+++ be/src/main/java/com/sharep/be/modules/issue/controller/IssueController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26d9ef83f7bf96348c13fb1b945a86bdd093ac3838c23720ebb9ae7ff490c5dd
-size 3645
+oid sha256:901e5d764a735293a60c4afdfb0ac9bbe49d258a65714e49363520b7c09b1599
+size 3639
