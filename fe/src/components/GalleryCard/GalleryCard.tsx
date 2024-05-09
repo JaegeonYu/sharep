162e9eac333146aec225795e6770212337504209
임서정 <sjytis14@naver.com>
2024-05-09 12:59:23 +0900

    [!HOTFIX] 진행 중인 이슈가 없는 경우 처리
    Former-commit-id: 856646b47c95ae229a301542de30fc7943831aeb

--- fe/src/components/GalleryCard/GalleryCard.tsx
+++ fe/src/components/GalleryCard/GalleryCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56fe56e2d34667db168604471e3ee56bbf042702db2b41030e7935b87c328fdc
-size 1839
+oid sha256:a355cd9acb15bc470c1cd54c36756dd622a3cc4934df3bd073ad19d3d60dd76d
+size 1814
