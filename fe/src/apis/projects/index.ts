30ba19bda36044aca6a15b2bea3e47577c19aee7
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-09 22:25:37 +0900

    [Refactor] 프로젝트의 모든 이슈 조회 분리
    - 모든 이슈 조회 -> [모든 이슈 간단 조회, 모든 이슈 상세 조회]로 분리
    
    
    Former-commit-id: 67e003bc942a04487e7736493018e1dbde8f75b1

--- fe/src/apis/projects/index.ts
+++ fe/src/apis/projects/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95e87880df23b0926a10fb2345494f1b8d20b0976d43c0b7773e3bd8f6da6ed8
-size 5819
+oid sha256:519356dbc7c8334353bf2ac5a390602fcf6c6f0598cc54259c084390ecdbdba5
+size 6190
--- fe/src/pages/CommitHistory/CommitHistory.tsx
+++ fe/src/pages/CommitHistory/CommitHistory.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50e813f384721dc138227a6af9b4ace685f336b7da4bef35e80de0ffcb2ffd20
-size 9309
+oid sha256:19f0c20cfefdce2b37d37599ab4bc7e816e118c103bbf1272728fff8b5b0f384
+size 9267
