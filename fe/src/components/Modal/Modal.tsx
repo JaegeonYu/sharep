b199ed8b7ee4b56a48b1b47e374734d670c25ee4
seojeong <sjytis14@naver.com>
2024-05-09 23:11:12 +0900

    [Fix] 새 프로젝트 생성 오류 수정
    Former-commit-id: 9c5ca4a5ec3abfa5882899a22de82540aa56bf8c

--- fe/src/components/Modal/Modal.tsx
+++ fe/src/components/Modal/Modal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce609089a16dfa67d76a4b54361e0b2a7d551b60601eab7985615640cae66f97
-size 4410
+oid sha256:561afc93b55df30093a85e5ff2e84fb88edc8cf8286907979ca5bf126b0cbd69
+size 4353
