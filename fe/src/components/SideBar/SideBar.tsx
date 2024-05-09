9ad0409a8a37a50b3d2945d2a6c625d94b3acfce
조성규 <jo41010@naver.com>
2024-05-09 16:00:43 +0900

    [Fix] login & regi btn
    Former-commit-id: 6b5f84c4a411ef7abaf7cee3e4e341af92b1731f

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:927268828c340a81fd67731e331a4d8989595b44254f7007ca53d70ec923c356
-size 8843
+oid sha256:171b71fc3c135ba49508dde1f14ef85ab8cd5aeefccf2528e23ad799f40595f4
+size 8869
--- fe/src/svg.d.ts
+++ fe/src/svg.d.ts
@@ -1,4 +1,3 @@
-declare module '*.svg' {
-  const content: React.FC<React.SVGProps<SVGElement>>;
-  export default content;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:999f1ef3f783a5a67ec2d836b74f9e55d23ec97055d0a2cc9c7bbb4ede8b9a98
+size 135
