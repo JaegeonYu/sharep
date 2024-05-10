26cc03b89081df4dc331a753e4e0b6242183bbf5
조성규 <jo41010@naver.com>
2024-05-10 17:51:54 +0900

    [Feat] infra layout
    Former-commit-id: 2bf417ff463e23864c296ffa38cc5511002dd336

--- fe/src/apis/projects/index.ts
+++ fe/src/apis/projects/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d5865bf7a4635c2444d826cecdb46531cdd14c059a0aed40f8654a62d066a0d
-size 6778
+oid sha256:9a0f3fb24f98375e662354ab5b2e7a0dee023ec711feaadcf5800b972d329506
+size 7073
