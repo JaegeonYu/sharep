41303ff24dc29af8b42ef2cae807592044092f2c
seojeong <sjytis14@naver.com>
2024-05-11 00:29:41 +0900

    [Design] gallerygridwrapper 카드 너비 수정, esc 입력 시 카드 편집 중단
    Former-commit-id: 28ea1672fd3df3bac89b424352d131e2cca88d9c

--- fe/src/components/GalleryGridWrapper/GalleryGridWrapperStyle.ts
+++ fe/src/components/GalleryGridWrapper/GalleryGridWrapperStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb6a177215ba463bed56de5211364e89ceaed22c5feb0a562094c6b4b21534e7
-size 2242
+oid sha256:9ffad8d58fc62afa4928f4d34d082b86a880983c9f102b939adb15e4580bfb68
+size 2121
