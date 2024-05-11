1d7a2bb5f9574f49066a30093db0849d681d78aa
seojeong <sjytis14@naver.com>
2024-05-12 01:00:24 +0900

    [Fix] 이슈 조회 후 정렬
    Former-commit-id: b11cc33198bfdb0a54e2927290d2c463c8dbc576

--- fe/src/pages/TeamMember/Subs/Kanban/Kanban.tsx
+++ fe/src/pages/TeamMember/Subs/Kanban/Kanban.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aad7ba4a59d6f61cfb2d45386b4d1bc4b8fa5eb660cea8021339e8f108679391
-size 4212
+oid sha256:abc3ba22b9bd54a0bc1d706e159f2c3ae34cbd514a52b257301d9f4f9c5bc19e
+size 4721
