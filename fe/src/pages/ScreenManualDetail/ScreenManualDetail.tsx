340d2bda8e67f2af6baa60d100479929a28b03b5
seojeong <sjytis14@naver.com>
2024-05-12 01:00:06 +0900

    [Fix] 이슈 조회 후 정렬
    Former-commit-id: dc2b6de879f8c77eeaeffa4ba0631dbd5fb610a0

--- fe/src/pages/ScreenManualDetail/ScreenManualDetail.tsx
+++ fe/src/pages/ScreenManualDetail/ScreenManualDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5050aa310398fc7704b5838223cec4c8859966ca57b17d81a8f13802cf7fbdc
-size 12677
+oid sha256:989d7897d673ff36b91ff7100fbb9fa693561050f1b7b885be457b249f7266bf
+size 13186
