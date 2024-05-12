8bfe0a3f073e986ccd7b1903139fe32a9edf8fcb
seojeong <sjytis14@naver.com>
2024-05-13 00:34:51 +0900

    [Feat] 팀원 추가하는 기능 추가, 작업 생성 시 이슈 드랍다운 구현 중
    Former-commit-id: 8cc3aed9f33ab791f36cdeb864aebcc6569e019c

--- fe/src/apis/projects/index.ts
+++ fe/src/apis/projects/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d59f3e27e33f801f37d67f6fd7b13922d0f33f222c2604b59b04ecac3659a2ea
-size 7693
+oid sha256:e9170d22f99e75a23ca517194e0b3bb65754229d33df12c8d4e9b98ce842daef
+size 7717
