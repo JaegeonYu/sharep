a9bdb422369b18792f31f022c1ee5985f642e1ab
Seungmin Lee <mehot98@gmail.com>
2024-05-12 19:38:17 +0900

    [Feat] 작업 조회 시 조건 and 연산으로 변환
    Former-commit-id: 60fca978a827ad07b490022ba44f6602f53f9486

--- be/src/main/java/com/sharep/be/modules/job/repository/JobRepositoryCustomImpl.java
+++ be/src/main/java/com/sharep/be/modules/job/repository/JobRepositoryCustomImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3866b4b73ee72e02ce08db09859d67341fc33e6afd74820cc1ef4d402a48be71
-size 3641
+oid sha256:3f57cdb77ce5b2247c64f3076f68d701917a7e7d7788e8a17053d2d99df29be1
+size 3186
