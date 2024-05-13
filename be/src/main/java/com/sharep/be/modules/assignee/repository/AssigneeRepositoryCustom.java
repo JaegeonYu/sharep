def7f566e96130bad62e0b4d4455146bf5970c14
이승민 <mehot98@gmail.com>
2024-05-13 12:08:45 +0900

    [Fix] 알림 보내기 로직 수정
    Former-commit-id: af8e3b2ae74ff9e188562e37d114f9cc21b56aec

--- be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryCustom.java
+++ be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryCustom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f67ae6c2ad153e843e14f4a7fecc69aee45b0bd9c1ddd98f19d87bbf7ecf2f7b
-size 519
+oid sha256:a0d20a8ec4ee87b3c4819508a74c5e065b741362f1845b78a3cf2f3e6300fdb5
+size 633
