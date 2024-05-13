99200e6f668a69e1170353e8d73ff18d216b95ea
이승민 <mehot98@gmail.com>
2024-05-13 17:45:56 +0900

    [Refactor] 담당자 레포지토리 구조 변경
    Former-commit-id: 8e6781ce528f749b8986b5e83bb0aa4be0556d74

---
+++ be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeJpaRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbe6c6ce0ed5eb8b73f5dd1a53fdb4c9ebc6c63859bea17684d4f880cdf3638b
+size 658
--- be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryCustom.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a0d20a8ec4ee87b3c4819508a74c5e065b741362f1845b78a3cf2f3e6300fdb5
-size 633
