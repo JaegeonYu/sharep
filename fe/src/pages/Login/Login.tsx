bd1c41988deae3c87f255656d656713aaa8e7e37
조성규 <jo41010@naver.com>
2024-05-14 10:47:16 +0900

    [Feat] infra alarm added
    Former-commit-id: 060554681e8d63680bc79f966e779ab1d9614504

--- fe/src/pages/Login/Login.tsx
+++ fe/src/pages/Login/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e65bfd23f10b38a742c303c5ce34d8df3287b1e7eba03a4f98bad62670d634e
-size 2650
+oid sha256:b42370e95b9debf9acf86955e2340c1bee358be399beead864265a67dfa3bf6b
+size 2591
