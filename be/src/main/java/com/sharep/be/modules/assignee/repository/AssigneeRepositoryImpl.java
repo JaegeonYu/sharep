dbb5662c4a2b8f3386e3911d08717e742feacb9e
이승민 <mehot98@gmail.com>
2024-05-14 12:51:19 +0900

    [Refactor] 선택된 사람들에게 알림보내기 로직 변경
    Former-commit-id: 7481a84d6e1d00747ba17d51cc66233e5705d373

--- be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryImpl.java
+++ be/src/main/java/com/sharep/be/modules/assignee/repository/AssigneeRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94e6088c84a42d8ff1a0dba5b46a76d7603c7338897447ba3d5878c3c9870cb3
-size 4646
+oid sha256:5130a739e593caf91fc2961987c6e3064ad62eb9b2c3283d4a6518811ffaee1f
+size 4868
--- be/src/main/java/com/sharep/be/modules/assignee/service/AssigneeRepository.java
+++ be/src/main/java/com/sharep/be/modules/assignee/service/AssigneeRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a773c2a4af4e42ae2681134ffb2fc987e472aeb3c7a59a8c51b45dce790d3819
-size 1124
+oid sha256:8f259e3451698b58eee129eb4c4b66799f9d691a81cf4c04707921b0162b5e5b
+size 1121
