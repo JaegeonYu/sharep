8ccf552ca03ecfff040d0d975064e61a54bf74f6
조성규 <jo41010@naver.com>
2024-05-14 16:48:06 +0900

    [Fix] infra alarm assignee fixed
    Former-commit-id: 527cfef26155bed0755001dea288a2954ef0313a

--- fe/src/types/components/GalleryCard.ts
+++ fe/src/types/components/GalleryCard.ts
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5bdaf550bc38feb4613fba13758fb55a496ac27bc78c0e95759d7982c5aa15da
-size 124
+import * as T from '@/types';
+export interface GalleryCardProps {
+  issue: T.API.SimpleIssue;
+  type: 'SCREEN' | 'INFRA';
+}
