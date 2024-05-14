438f16a8d18030bb842eaf19a00f9634b136ddfc
임서정 <sjytis14@naver.com>
2024-05-14 10:29:40 +0900

    [Fix] 진행 중인 이슈 리스트로 type 수정
    Former-commit-id: fc7e603d1642faf875bc922c2b58d6ec6b758fd8

--- fe/src/apis/projects/index.ts
+++ fe/src/apis/projects/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9470e566cdbe1e0bd4f4b838da5153fbd556003fe251ecbb7b929cca73b246a1
-size 8530
+oid sha256:5d48ea4dc349199145bc2003593115ab2efcd22761960d0e57e7b3914fedb118
+size 8502
