220ad7efe08c0baa3844a594c55143cfcf205f86
조성규 <jo41010@naver.com>
2024-05-14 16:48:06 +0900

    [Fix] infra alarm assignee fixed
    Former-commit-id: 0889e35fa628bcaae4bb954ed9403f1a3e582f8b

--- fe/src/components/Modal/Modal.tsx
+++ fe/src/components/Modal/Modal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d916df712c50ae4e7221f0a22e0dd6a6594420f10464efd4e6737fa95f8a8933
-size 7135
+oid sha256:0bb851239b7c8fdada4b36e719f5ad4e6406116a2470666e6beb9bebc1bc2f57
+size 6504
