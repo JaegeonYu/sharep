c6ab0a585a66bde98f862951e99c1cf0472f06a1
임서정 <sjytis14@naver.com>
2024-05-14 11:05:10 +0900

    [Design] sidebar 작업 히스토리, 생성 버튼 tooltip 추가
    Former-commit-id: 88787438b917537ebe8e9a669d3bd7c9fdbc6f3b

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6c16059616609ddca8cb6c5ce50b0b586f7bbbb062935f090b9422c693581de
-size 9994
+oid sha256:3ce952a71b5e16ae7f823242144d027613b3af164c49696f2d4bf57a59fd1fcc
+size 10331
