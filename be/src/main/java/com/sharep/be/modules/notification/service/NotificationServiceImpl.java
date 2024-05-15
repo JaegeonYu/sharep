c60fe2ea1b7b5ab091299f3f84e20e5e34a581e8
Seungmin Lee <mehot98@gmail.com>
2024-05-16 02:35:01 +0900

    [Fix] 공동작업 sse 수정
    Former-commit-id: 33dd44a119c81f1d72f2f7afb766eabf98fb8655

--- be/src/main/java/com/sharep/be/modules/notification/service/NotificationServiceImpl.java
+++ be/src/main/java/com/sharep/be/modules/notification/service/NotificationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7fb4c791c425bf535ef2aa4e66e39fe7a70125a25fd43077690463bcfb0b59
-size 6138
+oid sha256:8da3694b08b509d6dcc22f4da1a22f49c0730040e44b605c17adc0b6903a964e
+size 6440
