2299ff6d06a8abf9cfafaebd858d9b6c28888cbd
Seungmin Lee <mehot98@gmail.com>
2024-05-16 01:40:09 +0900

    [Fix] 알림 상태가 무조건 false였던 문제 개선
    Former-commit-id: 3a13e17fc52934ac9c27d0ef23dad37f4a030c7b

--- be/src/main/java/com/sharep/be/modules/notification/domain/NotificationMessage.java
+++ be/src/main/java/com/sharep/be/modules/notification/domain/NotificationMessage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d49254e556d0a737f23ddf1d8bff4e721b3e98853c7716276bbd5128f8b80e44
-size 1953
+oid sha256:d8f73a454a0440e81d2e042afd51a6748198bc3ae184ebaf7d75360ead1cf47e
+size 1969
