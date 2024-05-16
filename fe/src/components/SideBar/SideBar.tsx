51f753b13ee6deb75f9a968fd792361ece5c277c
조성규 <jo41010@naver.com>
2024-05-16 12:49:06 +0900

    [Fix] protected route
    Former-commit-id: 8f081ef2ded3ca7fa6b8c5a2b96a8296b9d4be44

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85db8f7b13685ba24ae9cf9b3ca7fa4bb4c0ba1a40ebb0916f966642bd62c245
-size 11409
+oid sha256:846a73eac87b7efed1508adfe31bc18cf74880a74ca06f35012c25503a5c3c78
+size 11505
--- fe/src/pages/Mypage/Mypage.tsx
+++ fe/src/pages/Mypage/Mypage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a8dc82167eeb7aad1a8ce733d5d76620b2eb5485ab8336db3154a4d6bb09b6c
-size 6039
+oid sha256:44ef19891afe6f9b0c927a9e3b7130e25e4c0fc90b29337fb5e419d37de397ef
+size 6063
