591b807c028840b059510776b363bb1ad3cd2ee8
김성제 <rlatjdwp0211@naver.com>
2024-05-16 13:22:44 +0900

    [Feat] Api 메뉴얼 readonly props 추가
    - readonly시 UI 및 함수에 readonly validation 추가
    
    
    Former-commit-id: 35a650efc7e42ca8b541f8151df1bee88b464bdf

--- fe/src/components/ApiManualTable/Subs/Row/Subs/TextAreaCel/TextAreaCel.tsx
+++ fe/src/components/ApiManualTable/Subs/Row/Subs/TextAreaCel/TextAreaCel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a15f02ef0da25df6fedbcfb4b9b99c4324eb5faf79ffda4f7700db4ebfada88c
-size 2183
+oid sha256:54e4cf3c6b0ab782306fe6a3f9f6c592cb26e27efec8add65d9f65def2d34489
+size 2334
