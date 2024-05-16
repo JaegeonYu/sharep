3d07034b7a59a1a3aee6432cfeef5c253820db0b
이승민 <mehot98@gmail.com>
2024-05-16 12:57:12 +0900

    [Fix] 공동작업 알림 수정
    Former-commit-id: a35aae0e359304fe5ac0508db1a94e31f55b5278

--- be/src/main/java/com/sharep/be/modules/notification/service/NotificationServiceImpl.java
+++ be/src/main/java/com/sharep/be/modules/notification/service/NotificationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1ef3ed24e599793e6a81d4db63bb6e9ba84f3e4985fc992593d65b210b2b4cf
-size 6472
+oid sha256:5a526ca9d4181db4ab7cd3f81fd60e6a1b3be3cd8a75256b5633ec22c7c49b4a
+size 6081
