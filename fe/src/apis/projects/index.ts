76154384bc5719401eb1243ecf19aa0158d7e632
김성제 <rlatjdwp0211@naver.com>
2024-05-16 17:49:21 +0900

    [Feat] SSE 연결 후 알람 전송 로직
    - 기능 명세서 수정 시 알람 전송 및 list 재 요청
    
    
    Former-commit-id: 5d3142f11dffaecb37ecb186014fb0508713d5c8

--- fe/src/apis/projects/index.ts
+++ fe/src/apis/projects/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b6bfb3bb3585b7c1f014d8f565a2fe1209c330cb896800744c10077d038a4ef
-size 9465
+oid sha256:4165920434b81f8fbf7f6ee37c9fe24d515c5b8f2a342c0ab08d9ca65befcd51
+size 9780
