39ebb9306a7bab4b02feb1a5baccb4307c75b3ac
이승민 <mehot98@gmail.com>
2024-05-16 16:04:42 +0900

    [Fix] 프로젝트 SSE 관련 로직 수정
    Former-commit-id: 7821099f5d152d7cd86033be4b38e70ea2d9f920

--- be/src/main/java/com/sharep/be/modules/notification/service/ProjectIdEmitterRepository.java
+++ be/src/main/java/com/sharep/be/modules/notification/service/ProjectIdEmitterRepository.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:635751c9fe2629455a541c872acd3da59708c6bd2e704ed3af461a51ec535ed6
-size 281
+package com.sharep.be.modules.notification.service;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface ProjectIdEmitterRepository {
+
+    void save(Long memberId, SseEmitter emitter);
+
+    void deleteById(Long memberId);
+
+    SseEmitter get(Long memberId);
+}
