f4dc54527db17f98e6d084292e72adaf7df767fe
김성제 <rlatjdwp0211@naver.com>
2024-05-16 13:23:30 +0900

    [Refactor] 메뉴얼 타입 수정
    - API 메뉴얼 readonly 추가
    
    - 기능 명세서 readonly 및 api props 추가
    
    
    Former-commit-id: 74e2a7f52ea549bf2c70d8d0f800857b267e4fa1

--- fe/src/types/components/FeatureManualTable.ts
+++ fe/src/types/components/FeatureManualTable.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:646ea1081e8f41d18171ae98a868a74af7a008b9a2432cc9e22a02db9cb8e36f
-size 724
+oid sha256:f7f661353317bbeb98d08e145d2160dd87bc242313c9e83acaa8a23f1b8949e1
+size 1107
