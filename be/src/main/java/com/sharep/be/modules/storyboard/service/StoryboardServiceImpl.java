6f5b312becd08f803b9ee41ffb21256c42aade91
jack <yjk9805@naver.com>
2024-05-16 16:39:11 +0900

    [Fix] storyboard @transactional 추가
    Former-commit-id: e3697a6561f40ce016de77863ceedd7f6ccfe168

--- be/src/main/java/com/sharep/be/modules/storyboard/service/StoryboardServiceImpl.java
+++ be/src/main/java/com/sharep/be/modules/storyboard/service/StoryboardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:302cdbf5a3c07b3c663fb4bd73abb035dde4b73a14467537da9f6db4faa12daa
-size 2281
+oid sha256:e2d4b6a4d8f90626a657f8dac5e5ce9ab94676c2003bfda90568c3c189b671d2
+size 2361
