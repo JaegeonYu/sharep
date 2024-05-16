2dbc0512e09f63cc3a39ba496387d5380d6ea6fd
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-16 09:13:28 +0900

    [Feat] FeatureManual
    - 타입 리펙터링 및 SSE 연결
    
    
    Former-commit-id: 7184ad9bbd8775d6d6be3c808d4f7f5a79ec6fb4

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:579b4ee3c9acf237a08b0c26f2deb03be129a60c5dee58fdf224a9481b418306
-size 11225
+oid sha256:ade778d78c575d1c46736744491fea24f42246bc0e53b8f0d8faaceb844e1323
+size 11228
