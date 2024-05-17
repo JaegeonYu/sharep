592dacf6277ac2f5731b5617fb66606a588537ca
조성규 <jo41010@naver.com>
2024-05-17 13:12:10 +0900

    [Fix] svg to png
    Former-commit-id: 3504d0c4cf4060c2fb14f1ca727c5ef93f667a9f

--- fe/src/components/Modal/Subs/JobCreationForm/JobCreationFormStyle.ts
+++ fe/src/components/Modal/Subs/JobCreationForm/JobCreationFormStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2a2432e8aac648c70451455b5e6563dcb4c8b87983f4d6b45fb37424a910ad6
-size 4609
+oid sha256:e05b3370325ea00589135123f6c24efba3c7b915a2e01c433d255a10282fdb10
+size 4796
--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9df090a8e4a024bfd24dbfe48b5ab4af416671f3ac556d7dc81fc64a31ec3e3a
-size 11924
+oid sha256:fa41ef40219fc0e4cf37805df27731c2e3cb1678c8504223384cf9f0832685d7
+size 12544
