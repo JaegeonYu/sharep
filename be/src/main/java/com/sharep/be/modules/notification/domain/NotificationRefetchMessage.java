c1b3fb561673b0aad6a4060e09cedbc44196f66b
이승민 <mehot98@gmail.com>
2024-05-17 17:30:35 +0900

    [Fix] API 명세서 조인 변경
    Former-commit-id: ff6c50ad8be67549b85b2c37f4c5eb5f83bbf9d7

--- be/src/main/java/com/sharep/be/modules/notification/domain/NotificationRefetchMessage.java
+++ be/src/main/java/com/sharep/be/modules/notification/domain/NotificationRefetchMessage.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0303af29f56b64794d7f93e1de2cb1357c2078573249e1ab1fba4c16bab6f871
-size 185
+package com.sharep.be.modules.notification.domain;
+
+public record NotificationRefetchMessage(String message) {
+    public NotificationRefetchMessage(){
+        this("refetch");
+    }
+}
--- be/src/main/java/com/sharep/be/modules/notification/service/ProjectIdEmitterRepository.java
+++ be/src/main/java/com/sharep/be/modules/notification/service/ProjectIdEmitterRepository.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c63d660d8b87cdae767de97304f507719709652894d535b94edcc3d42eb9f688
-size 299
+package com.sharep.be.modules.notification.service;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface ProjectIdEmitterRepository {
+
+    void save(Long memberId, SseEmitter emitter);
+
+    void deleteById(Long memberId);
+
+    SseEmitter get(Long memberId);
+}
