d59ff209ce6bc26e7229013f7bd50a942e1b02fb
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-17 09:15:34 +0900

    [Feat] 연결될 이슈 추가
    - UI 및 API 반영
    
    
    Former-commit-id: 4cc34555d914452a29c07ae1c75a104bd03d6227

--- fe/src/pages/TeamDashboard/TeamDashboard.tsx
+++ fe/src/pages/TeamDashboard/TeamDashboard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bf998af0b421324996f05490d9e55b789c19e6262bf573faf7adc3e25004f50
-size 3989
+oid sha256:ac42bba027cf4eba9a8b134b2efa7a29c338c9b3432addfdf991ff11158b81ae
+size 3966
