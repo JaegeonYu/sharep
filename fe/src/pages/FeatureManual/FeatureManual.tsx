a278b82d2bb77ab2e72d6b5673fe78fa1157aff5
김성제 <rlatjdwp0211@naver.com>
2024-05-17 13:34:47 +0900

    [Fix] SSE END_POINT 수정
    - DEV_END_POINT -> END_POINT
    
    
    Former-commit-id: d164100bb47cd9dada52f6f42556f48c34060004

--- fe/src/pages/FeatureManual/FeatureManual.tsx
+++ fe/src/pages/FeatureManual/FeatureManual.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c93dd2b90fed06df968c084973bc968832f9ac15b10e9a84c22c2e84b859162
-size 3136
+oid sha256:88b90a33a4c7701afc68c2fa30faa464aa1a2e87025675ccaa6413b62261135f
+size 3132
