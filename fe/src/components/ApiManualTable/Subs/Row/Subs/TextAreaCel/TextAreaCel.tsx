ebc1346e1173848e4a0b785ceb6f849088cd401d
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-18 02:35:26 +0900

    [Fix] API 메뉴얼 UI 버그
    - 마운트될 시 내부 컨텐츠보다 높이가 작은 이슈
    - type 수정
    
    
    Former-commit-id: 9ffe5368684e940a9305ed62923834584cf4a142

--- fe/src/components/ApiManualTable/Subs/Row/Subs/TextAreaCel/TextAreaCel.tsx
+++ fe/src/components/ApiManualTable/Subs/Row/Subs/TextAreaCel/TextAreaCel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd0619233bed0b7bfe9f40dd8c85ac6278c4dd6e8accad30ca6961aa1e3a2c77
-size 2730
+oid sha256:d4efbae562f718770ad52cb8afe17a8546d1cc50724b43aa97667d57165568c1
+size 2744
