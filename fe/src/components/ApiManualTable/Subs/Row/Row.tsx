9a2c4feff6f99873c1f7861db3b81e405c17fde8
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-18 18:17:44 +0900

    [Fix] 기능 명세서 텍스트 셀 수정
    - 서버 상태 변경 시 setter 실행
    
    
    Former-commit-id: a50154b1248a3ec00626366317d7cf66b32b76b4

--- fe/src/components/ApiManualTable/Subs/Row/Row.tsx
+++ fe/src/components/ApiManualTable/Subs/Row/Row.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c04ddf006ccb82efc4e71be227d1555a388346da91d1081bb15510557f58321d
-size 4079
+oid sha256:a72d872f5fe870f94385eed2561a998c7423e3cc4de36d5e7454978f56b39104
+size 4062
