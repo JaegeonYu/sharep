cb5a76067b00b301e536f13968a91a925e76f3ad
조성규 <jo41010@naver.com>
2024-05-18 18:13:19 +0900

    [Feat] symbol img
    Former-commit-id: 1fa8f77ca04ed0ffc4868be14a4e6c985f562dd3

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec857e0513ea064f100e4ad955bb007cb1174d87482e6c9e1d341038ce78e2b8
-size 866
+oid sha256:22942995e3b36de568f0d008c17bd6662520de4a608e7b83c0e96ff4c13f0d6f
+size 1009
