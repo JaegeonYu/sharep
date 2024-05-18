f08cfd8ab2d2721aa2c0480f2148e467312b5875
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-18 21:11:35 +0900

    [Fix] SSE connection time
    - 커넥션 타임 증가
    
    
    Former-commit-id: ba392c97c9ef042c3a3dbd30ac798cabcf5a2f02

--- fe/src/components/FeatureManualTable/Subs/Row/Subs/TextAreaCel/TextAreaCel.tsx
+++ fe/src/components/FeatureManualTable/Subs/Row/Subs/TextAreaCel/TextAreaCel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30f9bda7580c33f2853e82cbe429dcad90e2e4169a6694a655e425fbdbba40e2
-size 3018
+oid sha256:3322c6a73b7ab192f55e4ca32551eab9c65289f8b4a3892765a70877fd872afe
+size 3322
