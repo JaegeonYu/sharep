67012d299fc4aa059bbe5f54159eafd11003c745
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-19 20:42:41 +0900

    [Feat] 간트 차트 UI 생성
    - 피드백 반영 및 QA
    
    
    Former-commit-id: 76f7cfd702c053fd88af8097987b9ac4a91ef52f

--- fe/src/pages/TeamDashboard/TeamDashboard.tsx
+++ fe/src/pages/TeamDashboard/TeamDashboard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b16d97616d4225610e7a9a63e85db4e03d616e1ec338274fc99ac3511ae426e2
-size 4098
+oid sha256:0793e559c78a58a82596b2b96fa2bc8fee6ff206d1199ef793e2dcd07b743cab
+size 4104
