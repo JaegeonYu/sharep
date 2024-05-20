7d440ff22bfe56b633138c43556ec21aede91df9
이승민 <mehot98@gmail.com>
2024-05-20 09:26:27 +0900

    [[Docs] 리드미 추가
    Former-commit-id: 8b065fe46ea6efab5c14c7c0f4bec21062edb7a7

--- be/src/main/java/com/sharep/be/modules/exception/ProjectNotFoundException.java
+++ be/src/main/java/com/sharep/be/modules/exception/ProjectNotFoundException.java
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:30eaac4aef8145db6e786d1f46467111ae400e11b720a63dd941ea404ffa0122
-size 215
+package com.sharep.be.modules.exception;
+
+public class ProjectNotFoundException extends RuntimeException {
+
+    public ProjectNotFoundException() {
+        super("프로젝트를 찾을 수 없습니다.");
+    }
+}
